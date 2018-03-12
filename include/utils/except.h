cc55814e7ac0bc94ff72dddedf5b31df91ee29a4
kaizhou.li <kaizhou.li@seetatech.com>
2018-03-12 13:18:43 +0800

    add base utils

---
+++ include/utils/except.h
@@ -0,0 +1,31 @@
+//
+// Created by Lby on 2017/6/6.
+//
+
+#ifndef TENSORSTACK_UTILS_EXCEPT_H
+#define TENSORSTACK_UTILS_EXCEPT_H
+
+#include <exception>
+#include <string>
+
+#include "platform.h"
+
+#if TS_PLATFORM_CC_MSVC
+#define TS_NOEXCEPT
+#else
+#define TS_NOEXCEPT noexcept
+#endif
+
+namespace ts {
+    class Exception : public std::exception {
+    public:
+        explicit Exception(const std::string &message);
+
+        const char *what() const TS_NOEXCEPT override;
+
+    private:
+        std::string m_message;
+    };
+}
+
+#endif //TENSORSTACK_UTILS_EXCEPT_H
---
+++ src/utils/except.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Lby on 2017/6/6.
+//
+
+#include "utils/except.h"
+
+namespace ts {
+    Exception::Exception(const std::string &message)
+            : m_message(message) {}
+
+    const char *Exception::what() const TS_NOEXCEPT {
+        return m_message.c_str();
+    }
+}
