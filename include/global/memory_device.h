0fb7af0520cc7fb66e46e5acaf621008c4858002
kaizhou.li <kaizhou.li@seetatech.com>
2018-05-19 15:45:42 +0800

    add memory device support

---
+++ include/global/memory_device.h
@@ -0,0 +1,60 @@
+//
+// Created by seeta on 2018/5/19.
+//
+
+#ifndef TENSORSTACK_GLOBAL_DEVICE_MEMORY_H
+#define TENSORSTACK_GLOBAL_DEVICE_MEMORY_H
+
+#include <sstream>
+#include "utils/except.h"
+#include "device.h"
+
+namespace ts {
+    class NoMemoryDeviceException : public Exception {
+    public:
+        explicit NoMemoryDeviceException(const DeviceType &device_type)
+                : Exception(NoMemoryDeviceMessage(device_type)), m_no_memory_device_type(device_type) {
+        }
+
+        static std::string NoMemoryDeviceMessage(const DeviceType &device_type) {
+            std::ostringstream oss;
+            oss << "Compute device " << device_type
+                << " has no memory device registered. Please call RegisterMemoryDevice firstly.";
+            return oss.str();
+        }
+
+        const DeviceType &no_memory_device_type() const {
+            return m_no_memory_device_type;
+        }
+
+    private:
+        DeviceType m_no_memory_device_type;
+    };
+
+    /**
+     * Query memory device of compute device
+     * @param compute_device_type compute device type
+     * @return memory device
+     * @note May throw NoMemoryDeviceException
+     */
+    DeviceType QueryMemoryDevice(const DeviceType &compute_device_type);
+
+    /**
+     * Query memory device of compute device
+     * @param compute_device_type compute device
+     * @return memory device
+     * @note May throw NoMemoryDeviceException
+     * @note The returned device id is same with querying device
+     */
+    Device QueryMemoryDevice(const Device &compute_device);
+
+    /**
+     * register an map of compute device and memory device
+     * @param compute_device_type the compute device, such as CPU, GPU, CUBLAS, CUDNN, etc.
+     * @param memory_device_type the memory device, such as CPU and GPU.
+     */
+    void RegisterMemoryDevice(const DeviceType &compute_device_type, const DeviceType &memory_device_type) TS_NOEXCEPT;
+}
+
+
+#endif //TENSORSTACK_GLOBAL_DEVICE_MEMORY_H
---
+++ src/global/memory_device.cpp
@@ -0,0 +1,32 @@
+//
+// Created by seeta on 2018/5/19.
+//
+
+#include "global/memory_device.h"
+#include <map>
+
+namespace ts {
+
+    static std::map<DeviceType, DeviceType> &MapDeviceMemory() {
+        static std::map<DeviceType, DeviceType> map_device_memory;
+        return map_device_memory;
+    };
+
+    DeviceType QueryMemoryDevice(const DeviceType &compute_device_type) {
+        auto &map_device_memory = MapDeviceMemory();
+        auto memory_device = map_device_memory.find(compute_device_type);
+        if (memory_device != map_device_memory.end()) {
+            return memory_device->second;
+        }
+        throw NoMemoryDeviceException(compute_device_type);
+    }
+
+    Device QueryMemoryDevice(const Device &compute_device) {
+        return Device(QueryMemoryDevice(compute_device.type()), compute_device.id());
+    }
+
+    void RegisterMemoryDevice(const DeviceType &compute_device_type, const DeviceType &memory_device_type) {
+        auto &map_device_memory = MapDeviceMemory();
+        map_device_memory.insert(std::make_pair(compute_device_type, memory_device_type));
+    }
+}
