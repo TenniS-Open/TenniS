b3a3eb2984b09fee9202677f5e1401937a563173
kaizhou.li <kaizhou.li@seetatech.com>
2018-05-25 15:46:32 +0800

    self format the code

--- include/tensor/type.h
+++ include/tensor/type.h
@@ -32,145 +32,67 @@ namespace ts {
     inline int type_bytes(TYPE type) {
         static const auto FakeUsagePtr = (void *) (0x19910929);
         switch (type) {
-            case VOID:
-                return 0;
-            case INT8:
-                return 1;
-            case UINT8:
-                return 1;
-            case INT16:
-                return 2;
-            case UINT16:
-                return 2;
-            case INT32:
-                return 4;
-            case UINT32:
-                return 4;
-            case INT64:
-                return 8;
-            case UINT64:
-                return 8;
-            case FLOAT16:
-                return 2;
-            case FLOAT32:
-                return 4;
-            case FLOAT64:
-                return 6;
-            case PTR:
-                return sizeof(FakeUsagePtr);
-            case UNKNOWN8:
-                return 1;
-            case UNKNOWN16:
-                return 2;
-            case UNKNOWN32:
-                return 4;
-            case UNKNOWN64:
-                return 8;
-            case UNKNOWN128:
-                return 16;
+            case VOID: return 0;
+            case INT8: return 1;
+            case UINT8: return 1;
+            case INT16: return 2;
+            case UINT16: return 2;
+            case INT32: return 4;
+            case UINT32: return 4;
+            case INT64: return 8;
+            case UINT64: return 8;
+            case FLOAT16: return 2;
+            case FLOAT32: return 4;
+            case FLOAT64: return 6;
+            case PTR: return sizeof(FakeUsagePtr);
+            case UNKNOWN8: return 1;
+            case UNKNOWN16: return 2;
+            case UNKNOWN32: return 4;
+            case UNKNOWN64: return 8;
+            case UNKNOWN128: return 16;
         }
         return 0;
     }
 
     inline const char *type_str(TYPE type) {
         switch (type) {
-            case VOID:
-                return "void";
-            case INT8:
-                return "int8";
-            case UINT8:
-                return "uint8";
-            case INT16:
-                return "int64";
-            case UINT16:
-                return "uint64";
-            case INT32:
-                return "int32";
-            case UINT32:
-                return "uint32";
-            case INT64:
-                return "int64";
-            case UINT64:
-                return "uint64";
-            case FLOAT16:
-                return "float16";
-            case FLOAT32:
-                return "float32";
-            case FLOAT64:
-                return "float64";
-            case PTR:
-                return "pointer";
-            case UNKNOWN8:
-                return "unknown8";
-            case UNKNOWN16:
-                return "unknown16";
-            case UNKNOWN32:
-                return "unknown32";
-            case UNKNOWN64:
-                return "unknown64";
-            case UNKNOWN128:
-                return "unknown128";
+            case VOID: return "void";
+            case INT8: return "int8";
+            case UINT8: return "uint8";
+            case INT16: return "int64";
+            case UINT16: return "uint64";
+            case INT32: return "int32";
+            case UINT32: return "uint32";
+            case INT64: return "int64";
+            case UINT64: return "uint64";
+            case FLOAT16: return "float16";
+            case FLOAT32: return "float32";
+            case FLOAT64: return "float64";
+            case PTR: return "pointer";
+            case UNKNOWN8: return "unknown8";
+            case UNKNOWN16: return "unknown16";
+            case UNKNOWN32: return "unknown32";
+            case UNKNOWN64: return "unknown64";
+            case UNKNOWN128: return "unknown128";
         }
         return "unknown";
     }
 
-    template<TYPE T>
-    struct type {
-        using declare = void;
-    };
+    template <TYPE T> struct type { using declare = void; };
 
-    template<>
-    struct type<VOID> {
-        using declare = void;
-    };
-    template<>
-    struct type<INT8> {
-        using declare = int8_t;
-    };
-    template<>
-    struct type<UINT8> {
-        using declare = uint8_t;
-    };
-    template<>
-    struct type<INT16> {
-        using declare = int16_t;
-    };
-    template<>
-    struct type<UINT16> {
-        using declare = uint16_t;
-    };
-    template<>
-    struct type<INT32> {
-        using declare = int32_t;
-    };
-    template<>
-    struct type<UINT32> {
-        using declare = uint32_t;
-    };
-    template<>
-    struct type<INT64> {
-        using declare = int64_t;
-    };
-    template<>
-    struct type<UINT64> {
-        using declare = uint64_t;
-    };
-    template<>
-    struct type<FLOAT16> {
-        using declare = uint16_t;
-    }; ///< not specific doing method
-    template<>
-    struct type<FLOAT32> {
-        using declare = float;
-    };
-    template<>
-    struct type<FLOAT64> {
-        using declare = double;
-    };
-    template<>
-    struct type<PTR> {
-        using declare = void *;
-    };
+    template <> struct type<VOID> { using declare = void; };
+    template <> struct type<INT8> { using declare = int8_t; };
+    template <> struct type<UINT8> { using declare = uint8_t; };
+    template <> struct type<INT16> { using declare = int16_t; };
+    template <> struct type<UINT16> { using declare = uint16_t; };
+    template <> struct type<INT32> { using declare = int32_t; };
+    template <> struct type<UINT32> { using declare = uint32_t; };
+    template <> struct type<INT64> { using declare = int64_t; };
+    template <> struct type<UINT64> { using declare = uint64_t; };
+    template <> struct type<FLOAT16> { using declare = uint16_t; }; ///< not specific doing method
+    template <> struct type<FLOAT32> { using declare = float; };
+    template <> struct type<FLOAT64> { using declare = double; };
+    template <> struct type<PTR> { using declare = void*; };
 }
 
 
