40b8df68a110607e2590d84d25693c5e9a25f182
kaizhou.li <kaizhou.li@seetatech.com>
2018-05-28 21:25:16 +0800

    fix shape to stand alone version

--- include/tensor/tensor.h
+++ include/tensor/tensor.h
@@ -8,22 +8,37 @@
 #include "mem/memory.h"
 #include "type.h"
 #include <vector>
-#include <stack/stack.h>
+#include <mem/controller.h>
 
 namespace ts {
     class tensor {
     public:
         class shape {
+        public:
+            shape() {}
+            shape(const std::vector<int> &sizes) : m_sizes(sizes) {}
+            shape(const std::vector<int> &sizes, const std::string &layout) : m_sizes(sizes), m_layout(layout) {}
+
+            size_t dims() const {return m_sizes.size();}
+            const std::vector<int> &sizes() const {return m_sizes;}
+            int operator[](size_t axis) const {return m_sizes[axis];}
+            const std::string &layout() const {return m_layout;}
+
+        private:
+            std::vector<int> m_sizes = {};
             std::string m_layout; ///< NCHW or NHWC
-            std::vector<int> m_shape = {};
         };
+        tensor(MemoryController::shared &controller);   // allocate memory from controller
+        tensor(const Device &device, const std::vector<int> &shape) : m_memory(device, (size_t)(count(shape))), m_shape(shape) {};   // allocate memory from device
 
         const Device &device() const { return m_memory.device(); }
 
         TYPE type() const { return m_type; }
 
+        int count() const;
+        static int count(const std::vector<int> &shape);
+
     private:
-        tensor(stack *s);   // allocate memory from stack
 
         Memory m_memory;
         TYPE m_type;
