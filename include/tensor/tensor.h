887803b266e2fa0604c4a5c862ae3a41ed81547a
kaizhou.li <kaizhou.li@seetatech.com>
2018-06-02 20:36:46 +0800

    rename tensor to Tensor

--- include/tensor/tensor.h
+++ include/tensor/tensor.h
@@ -11,70 +11,79 @@
 #include <mem/controller.h>
 
 namespace ts {
+    using Shape = std::vector<int>;
     class tensor {
     public:
-        class prototype {
+        class Prototype {
         public:
-            prototype() {}
+            Prototype() {}
 
-            prototype(const std::vector<int> &sizes) : m_sizes(sizes) {}
+            Prototype(const Shape &sizes) : m_sizes(sizes) {}
 
-            prototype(TYPE type, const std::vector<int> &sizes) : m_type(type), m_sizes(sizes) {}
+            Prototype(TYPE type, const Shape &sizes) : m_type(type), m_sizes(sizes) {}
 
-            explicit prototype(TYPE type) : m_type(type) {}
+            explicit Prototype(TYPE type) : m_type(type) {}
 
             TYPE type() const { return m_type; }
 
             size_t dims() const { return m_sizes.size(); }
 
-            const std::vector<int> &sizes() const { return m_sizes; }
+            const Shape &sizes() const { return m_sizes; }
 
             int type_bytes() const { return ts::type_bytes(m_type); }
 
             int count() const { return count(m_sizes); };
 
-            static int count(const std::vector<int> &_shape) {
+            static int count(const Shape &shape) {
                 int prod = 1;
-                for (int _size : _shape) prod *= _size;
+                for (int _size : shape) prod *= _size;
                 return prod;
             }
 
         private:
             TYPE m_type = VOID;
-            std::vector<int> m_sizes = {};
+            std::vector<int> m_sizes = {};  ///< in reversed mode
             // std::string m_layout; ///< NCHW or NHWC
         };
 
         tensor(MemoryController::shared &controller, TYPE type,
-               const std::vector<int> &_shape);   // allocate memory from controller
+               const Shape &_shape);   // allocate memory from controller
 
-        tensor(const Device &device, TYPE type, const std::vector<int> &_shape);
+        tensor(const Device &device, TYPE type, const Shape &_shape);
 
-        tensor(TYPE type, const std::vector<int> &_shape);
+        tensor(TYPE type, const Shape &_shape);
 
-        tensor(MemoryController::shared &controller, const prototype &proto);   // allocate memory from controller
+        tensor(MemoryController::shared &controller, const Prototype &proto);   // allocate memory from controller
 
-        tensor(const Device &device, const prototype &proto);
+        tensor(const Device &device, const Prototype &proto);
 
-        explicit tensor(const prototype &proto);
+        explicit tensor(const Prototype &proto);
 
         const Device &device() const { return m_memory.device(); }
 
         TYPE type() const { return m_proto.type(); }
+
+        size_t dims() const { return m_proto.dims(); }
+
+        const Shape &sizes() const { return m_proto.sizes(); }
+
+        int count() const { return m_proto.count(); };
+
+        const Prototype &proto() const {return m_proto;}
 
         void *data() { return m_memory.data(); }
 
         const void *data() const { return m_memory.data(); }
 
         template<typename T>
-        void *data() { return m_memory.data<T>(); }
+        T *data() { return m_memory.data<T>(); }
 
         template<typename T>
-        const void *data() const { return m_memory.data<T>(); }
+        const T *data() const { return m_memory.data<T>(); }
 
     private:
         Memory m_memory;
-        prototype m_proto;
+        Prototype m_proto;
     };
 }
 
--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -5,24 +5,24 @@
 #include "tensor/tensor.h"
 
 namespace ts {
-    tensor::tensor(MemoryController::shared &controller, TYPE type, const std::vector<int> &_shape)
-            : tensor(controller, prototype(type, _shape)) {}
+    tensor::tensor(MemoryController::shared &controller, TYPE type, const Shape &_shape)
+            : tensor(controller, Prototype(type, _shape)) {}
 
-    tensor::tensor(const Device &device, TYPE type, const std::vector<int> &_shape)
-            : tensor(device, prototype(type, _shape)) {}
+    tensor::tensor(const Device &device, TYPE type, const Shape &_shape)
+            : tensor(device, Prototype(type, _shape)) {}
 
-    tensor::tensor(TYPE type, const std::vector<int> &_shape)
-            : tensor(prototype(type, _shape)) {}
+    tensor::tensor(TYPE type, const Shape &_shape)
+            : tensor(Prototype(type, _shape)) {}
 
-    tensor::tensor(MemoryController::shared &controller, const tensor::prototype &proto)
+    tensor::tensor(MemoryController::shared &controller, const tensor::Prototype &proto)
         : m_memory(controller->alloc(static_cast<size_t>(proto.count() * proto.type_bytes())))
         , m_proto(proto) {}
 
-    tensor::tensor(const Device &device, const tensor::prototype &proto)
+    tensor::tensor(const Device &device, const tensor::Prototype &proto)
             : m_memory(device, static_cast<size_t>(proto.count() * proto.type_bytes()))
             , m_proto(proto) {}
 
-    tensor::tensor(const tensor::prototype &proto)
+    tensor::tensor(const tensor::Prototype &proto)
             : m_memory(static_cast<size_t>(proto.count() * proto.type_bytes()))
             , m_proto(proto) {}
 }
