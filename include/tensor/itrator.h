e9e2ee359a9bfd72a0e9ae4a0c0f42fca3b7e65b
kaizhou.li <kaizhou.li@seetatech.com>
2018-06-29 17:45:14 +0800

    update CMakeLists.txt for output setting

---
+++ include/tensor/itrator.h
@@ -0,0 +1,16 @@
+//
+// Created by seeta on 2018/5/31.
+//
+
+#ifndef TENSORSTACK_ITRATOR_H
+#define TENSORSTACK_ITRATOR_H
+
+
+
+class itrator {
+
+};
+
+
+
+#endif //TENSORSTACK_ITRATOR_H
---
+++ src/tensor/itrator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by seeta on 2018/5/31.
+//
+
+#include "itrator.h"
