1dc1a0d56634926ed2dde16108aeb95b0c6be032
kaizhou.li <kaizhou.li@seetatech.com>
2018-10-16 21:26:43 +0800

    add setup content

--- src/global/setup.cpp
+++ src/global/setup.cpp
@@ -3,10 +3,12 @@
 //
 
 #include "global/setup.h"
+#include "kernels/cpu/memory_cpu.h"
 
 namespace ts {
     void setup() {
         // may do some setup
+        cpu_allocator(0, 0, nullptr);
     }
 }
 
