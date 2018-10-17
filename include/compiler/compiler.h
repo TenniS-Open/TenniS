ad05a70ac0ebe0ff9914bb957fe6a51f410d88a6
kaizhou.li <kaizhou.li@seetatech.com>
2018-10-17 22:46:33 +0800

    add compiler

---
+++ include/compiler/compiler.h
@@ -0,0 +1,36 @@
+//
+// Created by kier on 2018/10/17.
+//
+
+#ifndef TENSORSTACK_COMPILER_COMPILER_H
+#define TENSORSTACK_COMPILER_COMPILER_H
+
+#include "runtime/instruction.h"
+#include "module/graph.h"
+
+namespace ts {
+
+    class InstructionBlock
+    {
+    public:
+        int nargs = 0;
+        int nresults = 0;
+        std::vector<Instruction::shared> instructions;
+    };
+
+    class Compiler {
+    public:
+        using self = Compiler;
+
+        explicit Compiler(const ComputingDevice &computing_device);
+
+        InstructionBlock compile(const std::vector<Node> &inputs, const std::vector<Node> &outputs);
+
+    private:
+        ComputingDevice m_computing_device;
+    };
+
+}
+
+
+#endif //TENSORSTACK_COMPILER_COMPILER_H
---
+++ src/compiler/compiler.cpp
@@ -0,0 +1,92 @@
+//
+// Created by kier on 2018/10/17.
+//
+
+#include "compiler/compiler.h"
+#include "module/module.h"
+#include "runtime/instruction.h"
+
+#include <deque>
+#include <set>
+#include <map>
+#include "runtime/instruction/istack.h"
+#include "global/operator_factory.h"
+
+
+namespace ts {
+
+    Compiler::Compiler(const ComputingDevice &computing_device)
+        : m_computing_device(computing_device) {
+
+    }
+
+    InstructionBlock Compiler::compile(const std::vector<Node> &inputs, const std::vector<Node> &outputs) {
+        InstructionBlock block;
+        block.nargs = int(inputs.size());
+        block.nresults = int(outputs.size());
+
+        // transform graph, make other graph from different framework to TS framework
+
+        // compile graph, check node to computing device support nodes
+
+        // convert graph to instructions
+        std::deque<Node> simulator;
+        for (auto &node : outputs) simulator.push_back(node);
+        std::map<Node, size_t> working_inptus;
+
+        auto add_node_to_simulator = [&](Node node) {
+            auto op = node.ref<OP>();
+            size_t i = simulator.size();
+            if (op.op == OP::IN) {
+                auto it = working_inptus.find(node);
+                if (it == working_inptus.end()) {
+                    working_inptus.insert(std::make_pair(node, i));
+                } else {
+                    if (i < it->second) it->second = i;
+                }
+            }
+            simulator.push_back(node);
+        };
+
+        auto pop_simulator = [&]() {
+            simulator.pop_back();
+        };
+
+        auto ring_shift_right_simulator = [&]() {
+            simulator.push_front(simulator.back());
+            simulator.pop_back();
+        };
+
+        while (!simulator.empty()) {
+            if (simulator.size() == working_inptus.size()) {
+                // check if all reminds node are input nodes
+                std::set<Node> simulator_nodes;
+                for (auto &node : simulator) simulator_nodes.insert(node);
+                if (simulator_nodes.size() == working_inptus.size()) break;
+            }
+            auto node = simulator.back();
+            auto op = node.ref<OP>();
+            if (op.op == OP::IN) {
+                auto it = working_inptus.find(node);
+                if (it != working_inptus.end()) {   // input already working in simulator
+                    // add copy inst
+                    block.instructions.push_back(instruction::Stack::push(int(it->second)));
+                    pop_simulator();
+                    break;
+                } else {
+                    // add left roll stack
+                    block.instructions.push_back(instruction::Stack::ring_shift_left());
+                    ring_shift_right_simulator();
+                    break;
+                }
+            }
+            // query operator
+            auto creator = QueryOperatorCreator(op.op);
+            if (creator == nullptr) throw Exception("Not supported operator " + op.op);
+            block.instructions.push_back(std::make_shared<OperatorInstruction>(creator(), node.inputs().size(), node.outputs().size()));
+            for (auto &input : node.inputs()) add_node_to_simulator(input);
+        }
+
+        return block;
+    }
+}
