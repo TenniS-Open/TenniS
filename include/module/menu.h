1ad3bd423c08b7ee63551794c4dff3eeb37f5706
kaizhou.li <kaizhou.li@seetatech.com>
2018-10-31 21:43:43 +0800

    add documents

--- include/module/menu.h
+++ include/module/menu.h
@@ -10,8 +10,31 @@
 
 namespace ts {
     namespace bubble {
+        /**
+         * get Parameter node
+         * @param name Node name
+         * @return new Node belonging to context-Graph
+         * @note Must call `ts::ctx::bind<Graph>` to bind context firstly
+         */
         Node param(const std::string &name);
+
+        /**
+         * get Parameter node
+         * @param name Node name
+         * @param op_name Operator name
+         * @param inputs Input nodes
+         * @return new Node belonging to context-Graph
+         * @note Must call `ts::ctx::bind<Graph>` to bind context firstly
+         */
         Node op(const std::string &name, const std::string &op_name, const std::vector<Node> &inputs);
+
+        /**
+         * get Parameter node
+         * @param name Node name
+         * @param value the data value
+         * @return new Node belonging to context-Graph
+         * @note Must call `ts::ctx::bind<Graph>` to bind context firstly
+         */
         Node data(const std::string &name, const Tensor &value);
     }
 }
