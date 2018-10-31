2243e71a16ec04b0016378e048061f32058a130b
kaizhou.li <kaizhou.li@seetatech.com>
2018-10-31 22:28:04 +0800

    split edit_distance to box.cpp

---
+++ include/utils/box.h
@@ -0,0 +1,15 @@
+//
+// Created by kier on 2018/10/31.
+//
+
+#ifndef TENSORSTACK_UTILS_BOX_H
+#define TENSORSTACK_UTILS_BOX_H
+
+#include <string>
+
+namespace ts {
+    int edit_distance(const std::string &lhs, const std::string &rhs);
+}
+
+
+#endif //TENSORSTACK_UTILS_BOX_H
--- src/runtime/workbench.cpp
+++ src/runtime/workbench.cpp
@@ -9,6 +9,7 @@
 #include "runtime/workbench.h"
 #include "global/memory_device.h"
 #include "compiler/compiler.h"
+#include "utils/box.h"
 
 namespace ts {
     Workbench::Workbench(const ComputingDevice &device)
@@ -134,46 +135,6 @@ namespace ts {
             throw Exception("Input index out of range. with index=" + std::to_string(slot));
         }
         return m_inputs[slot];
-    }
-
-    template<typename T>
-    static inline T min(T a, T b, T c) {
-        return std::min<T>(std::min<T>(a, b), c);
-    }
-
-    static inline int edit_distance(const std::string &lhs, const std::string &rhs) {
-        const size_t M = lhs.length();  // rows
-        const size_t N = rhs.length();  // cols
-
-        if (M == 0) return int(N);
-        if (N == 0) return int(M);
-
-        std::unique_ptr<int[]> dist(new int[M * N]);
-#define __EDIT_DIST(m, n) (dist[(m) * N + (n)])
-        __EDIT_DIST(0, 0) = lhs[0] == rhs[0] ? 0 : 2;
-        for (size_t n = 1; n < N; ++n) {
-            __EDIT_DIST(0, n) = __EDIT_DIST(0, n - 1) + 1;
-        }
-        for (size_t m = 1; m < M; ++m) {
-            __EDIT_DIST(m, 0) = __EDIT_DIST(m - 1, 0) + 1;
-        }
-        for (size_t m = 1; m < M; ++m) {
-            for (size_t n = 1; n < N; ++n) {
-                if (lhs[m] == rhs[n]) {
-                    __EDIT_DIST(m, n) = min(
-                            __EDIT_DIST(m - 1, n),
-                            __EDIT_DIST(m, n - 1),
-                            __EDIT_DIST(m - 1, n - 1));
-                } else {
-                    __EDIT_DIST(m, n) = min(
-                            __EDIT_DIST(m - 1, n) + 1,
-                            __EDIT_DIST(m, n - 1) + 1,
-                            __EDIT_DIST(m - 1, n - 1) + 2);
-                }
-            }
-        }
-        return dist[M * N - 1];
-#undef __EDIT_DIST
     }
 
     static std::string fuzzy_name(const Workbench::map<std::string, int> &map_name_slot, const std::string &name) {
