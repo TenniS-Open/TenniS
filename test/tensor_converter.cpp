1ae038c6fc4a5d4e7ab428a242b6921519e88f21
kaizhou.li <kaizhou.li@seetatech.com>
2018-10-31 12:54:08 +0800

    add cast test

--- test/tensor_converter.cpp
+++ test/tensor_converter.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <core/tensor_converter.h>
 #include <global/setup.h>
+#include <core/tensor.h>
 
 int main() {
     using namespace ts;
@@ -15,5 +16,11 @@ int main() {
     auto a = tensor::from(str);
 
     std::cout << str << " vs. " << tensor::to_string(a) << std::endl;
+
+    Tensor ti(ts::FLOAT32, {1});
+    ti.data<float>()[0] = 12.3f;
+    Tensor tf = tensor::cast(ts::INT32, ti);
+
+    std::cout << tf.data<int>()[0] << std::endl;
 
 }
