837ab826bc0e3d08679bbe6bafea587bfc17ad2d
kaizhou.li <kaizhou.li@seetatech.com>
2018-10-31 17:21:19 +0800

    rename dtype

--- src/core/tensor_converter.cpp
+++ src/core/tensor_converter.cpp
@@ -28,27 +28,27 @@ namespace ts {
             return std::string(cpu_value.data<char>(), size_t(length));
         }
 
-        template <DTYPE TYPE_DST, DTYPE TYPE_SRC>
-        static void type_cast(typename dtype<TYPE_DST>::declare *dst, const typename dtype<TYPE_SRC>::declare *src, size_t size) {
+        template <DTYPE DTYPE_DST, DTYPE DTYPE_SRC>
+        static void type_cast(typename dtype<DTYPE_DST>::declare *dst, const typename dtype<DTYPE_SRC>::declare *src, size_t size) {
             size_t i = 0;
             for (; i + 4 <= size; i += 4) {
-                *dst = static_cast<typename dtype<TYPE_DST>::declare>(*src); ++dst; ++src;
-                *dst = static_cast<typename dtype<TYPE_DST>::declare>(*src); ++dst; ++src;
-                *dst = static_cast<typename dtype<TYPE_DST>::declare>(*src); ++dst; ++src;
-                *dst = static_cast<typename dtype<TYPE_DST>::declare>(*src); ++dst; ++src;
+                *dst = static_cast<typename dtype<DTYPE_DST>::declare>(*src); ++dst; ++src;
+                *dst = static_cast<typename dtype<DTYPE_DST>::declare>(*src); ++dst; ++src;
+                *dst = static_cast<typename dtype<DTYPE_DST>::declare>(*src); ++dst; ++src;
+                *dst = static_cast<typename dtype<DTYPE_DST>::declare>(*src); ++dst; ++src;
             }
             for (; i < size; ++i) {
-                *dst = static_cast<typename dtype<TYPE_DST>::declare>(*src); ++dst; ++src;
+                *dst = static_cast<typename dtype<DTYPE_DST>::declare>(*src); ++dst; ++src;
             }
         };
 
-        template <DTYPE TYPE_SRC>
-        static void type_cast_to(void *dst, DTYPE dst_type, const typename dtype<TYPE_SRC>::declare *src, size_t size) {
-            switch (dst_type)
+        template <DTYPE DTYPE_SRC>
+        static void type_cast_to(void *dst, DTYPE dst_dtype, const typename dtype<DTYPE_SRC>::declare *src, size_t size) {
+            switch (dst_dtype)
             {
-                default: throw Exception( std::string("Can not convert dtype ") + type_str(TYPE_SRC) + " to " + type_str(dst_type));
+                default: throw Exception( std::string("Can not convert dtype ") + type_str(DTYPE_SRC) + " to " + type_str(dst_dtype));
 #define __CASE_TYPE_CALL_TYPE_CAST(__type__) \
-                case __type__: type_cast<__type__, TYPE_SRC>(reinterpret_cast<typename dtype<__type__>::declare *>(dst), src, size); break;
+                case __type__: type_cast<__type__, DTYPE_SRC>(reinterpret_cast<typename dtype<__type__>::declare *>(dst), src, size); break;
                 __CASE_TYPE_CALL_TYPE_CAST(INT8)
                 __CASE_TYPE_CALL_TYPE_CAST(UINT8)
                 __CASE_TYPE_CALL_TYPE_CAST(INT16)
@@ -67,12 +67,12 @@ namespace ts {
             }
         }
 
-        static void type_cast_to_from(void *dst, DTYPE dst_type, const void *src, DTYPE src_type, size_t size) {
-            switch (src_type)
+        static void type_cast_to_from(void *dst, DTYPE dst_dtype, const void *src, DTYPE src_dtype, size_t size) {
+            switch (src_dtype)
             {
-                default: throw Exception( std::string("Can not convert dtype ") + type_str(src_type) + " to " + type_str(dst_type));
+                default: throw Exception( std::string("Can not convert dtype ") + type_str(src_dtype) + " to " + type_str(dst_dtype));
 #define __CASE_TYPE_CALL_TYPE_CAST_TO(__type__) \
-                case __type__: type_cast_to<__type__>(dst, dst_type, reinterpret_cast<const typename dtype<__type__>::declare *>(src), size); break;
+                case __type__: type_cast_to<__type__>(dst, dst_dtype, reinterpret_cast<const typename dtype<__type__>::declare *>(src), size); break;
                 __CASE_TYPE_CALL_TYPE_CAST_TO(INT8)
                 __CASE_TYPE_CALL_TYPE_CAST_TO(UINT8)
                 __CASE_TYPE_CALL_TYPE_CAST_TO(INT16)
