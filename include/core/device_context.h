4818b6ca313dd8954ae077f246c7d2ec7a95700a
kaizhou.li <kaizhou.li@seetatech.com>
2018-11-02 21:27:09 +0800

    add device_context

---
+++ include/core/device_context.h
@@ -0,0 +1,14 @@
+//
+// Created by kier on 2018/11/2.
+//
+
+#ifndef TENSORSTACK_DEVICE_CONTEXT_H
+#define TENSORSTACK_DEVICE_CONTEXT_H
+
+
+class device_context {
+
+};
+
+
+#endif //TENSORSTACK_DEVICE_CONTEXT_H
---
+++ src/core/device_context.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kier on 2018/11/2.
+//
+
+#include "core/device_context.h"
