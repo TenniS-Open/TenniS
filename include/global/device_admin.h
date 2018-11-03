33c43c3fe1123d665f4ebcdaccc83387d236e47f
kaizhou.li <kaizhou.li@seetatech.com>
2018-11-03 22:48:42 +0800

    rewrite declaration

--- include/global/device_admin.h
+++ include/global/device_admin.h
@@ -11,15 +11,49 @@
 
 namespace ts {
 
+    /**
+     * Action on device
+     */
     enum DeviceAction {
-        DEVICE_INITIALIZATION,
-        DEVICE_FINALIZATION,
+        DEVICE_INITIALIZATION,  ///< initialize a handle on given device
+        DEVICE_FINALIZATION,    ///< finalize a handle on given device
     };
 
     /**
      * DeviceAdmin, initialize or finalize device shang xia wen
      */
     using DeviceAdmin = std::function<void(DeviceHandle **handle, int device_id, DeviceAction action)>;
+
+
+    /**
+     * Example of DeviceAdmin
+     * @param handle Pointer to the pointer of handle, ready to be initialized of finalized, which controlled by `action`
+     * @param device_id admin device id
+     * @param action @sa DeviceAction
+     */
+    void DeviceAdminDeclaration(DeviceHandle **handle, int device_id, DeviceAction action);
+
+    /**
+     * Query device admin
+     * @param device querying computing device
+     * @return DeviceAdmin
+     * @note supporting called by threads without calling @sa RegisterDeviceAdmin
+     * @note the query device should be computing device
+     */
+    DeviceAdmin QueryDeviceAdmin(const DeviceType &device) TS_NOEXCEPT;
+
+    /**
+     * Register allocator for specific device type
+     * @param device_type specific @sa DeviceType
+     * @param allocator setting allocator
+     * @note only can be called before running @sa QueryDeviceAdmin
+     */
+    void RegisterDeviceAdmin(const DeviceType &device_type, const DeviceAdmin &allocator) TS_NOEXCEPT;
+
+    /**
+     * No details for this API, so DO NOT call it
+     */
+    void ClearRegisteredDeviceAdmin();
 }
 
 
--- include/runtime/instruction/instruction_factory.h
+++ include/runtime/instruction/instruction_factory.h
@@ -5,9 +5,44 @@
 #ifndef TENSORSTACK_RUNTIME_INSTRUCTION_INSTRUCTION_FACTORY_H
 #define TENSORSTACK_RUNTIME_INSTRUCTION_INSTRUCTION_FACTORY_H
 
+#include <functional>
+
+#include "../instruction.h"
+#include "module/graph.h"
 
 namespace ts {
+    // TODO: add instruction factory, query instruction by name
+    // Those instructions are cross computing and memory device operator
+    using InstructionBuilder = std::function<std::vector<Instruction::shared>(const Node &node)>;
 
+    /**
+     * Example of InstructionBuilder
+     * @param node node ready to convert to instruction
+     * @return an serial of instructions, those can calculate node
+     */
+    std::vector<Instruction::shared> InstructionBuilderDeclaration(const Node &node);
+
+    /**
+     * Query instruction builder of specific op
+     * @param op querying op
+     * @return InstructionBuilder
+     * @note supporting called by threads without calling @sa RegisterInstructionBuilder
+     * @note the query should be the Bubble.op
+     */
+    InstructionBuilder QueryInstructionBuilder(const std::string &op) TS_NOEXCEPT;
+
+    /**
+     * Register InstructionBuilder for specific op
+     * @param op specific op name @sa Bubble
+     * @param builder instruction builder
+     * @note only can be called before running @sa QueryInstructionBuilder
+     */
+    void RegisterInstructionBuilder(const std::string &op, const InstructionBuilder &builder) TS_NOEXCEPT;
+
+    /**
+     * No details for this API, so DO NOT call it
+     */
+    void ClearRegisterInstructionBuilder();
 }
 
 
