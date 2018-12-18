51d63c389dee1b289533ba448ba47de435b6b899
kaizhou.li <kaizhou.li@seetatech.com>
2018-12-18 13:02:23 +0800

    add menu

--- python/tensorstack/menu.py
+++ python/tensorstack/menu.py
@@ -8,16 +8,34 @@ from node import Node
 
 
 def param(name):
+    # type: (str) -> Node
     return Node(op=Node.Parameter, name=name)
 
 
 def op(name, op_name, inputs, output_count=1):
+    # type: (str, str, list[Node], int) -> Node
     node = Node(op=op_name, name=name, output_count=output_count)
     Node.Link(node, inputs=inputs)
     return node
 
 
 def data(name, value):
+    # type: (str, object) -> Node
     node = Node(op=Node.Const, name=name)
     node.set("value", value)
+    return node
+
+
+def field(name, input, offset):
+    # type: (str, Node, int) -> Node
+    node = Node(op="_field", name=name)
+    node.set("offset", offset)
+    Node.Link(node=node, inputs=[input])
+    return node
+
+
+def pack(name, inputs):
+    # type: (str, list[Node]) -> Node
+    node = Node(op="_pack", name=name)
+    Node.Link(node=node, inputs=inputs)
     return node
