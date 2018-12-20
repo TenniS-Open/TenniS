152d6991d6bab3114f8be5efea6a5b56e3898eac
kaizhou.li <kaizhou.li@seetatech.com>
2018-12-20 11:22:56 +0800

    add sink number support

---
+++ test/sink.cpp
@@ -0,0 +1,96 @@
+//
+// Created by kier on 2018/12/19.
+//
+
+#include "core/sink.h"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+
+int find_first_i(int i) {
+    int p = 0;
+    while (i) {
+        ++p;
+        i >>= 1;
+    }
+    return p;
+};
+
+void plot_list() {
+    int i = 0;
+    while (true) {
+        auto p = find_first_i(i);
+        std::cout << p << ", ";
+        ++i;
+        if (i % 16 == 0) std::cout << std::endl;
+        if (p == 8) break;
+    }
+    std::cout << std::endl;
+}
+
+std::string to_bin_string(int32_t i) {
+    std::ostringstream oss;
+    oss << std::bitset<sizeof(int32_t)*8>(i);
+    return oss.str();
+}
+
+void plot_block(const std::string &str, const std::vector<int> &blocks) {
+    std::vector<std::pair<int, int>> ranges;
+    int left = 0;
+    for (auto &block : blocks) {
+        ranges.emplace_back(std::make_pair(left, block));
+        left += block;
+    }
+    bool first = true;
+    for (auto &range : ranges) {
+        if (first) first = false;
+        else std::cout << " ";
+        std::cout << str.substr(range.first, range.second);
+    }
+    std::cout << std::endl;
+}
+
+
+void test() {
+    int count = 0;
+    int match = 0;
+
+    int i = 0;
+    while (true) {
+        if (i & 0xffffff00) break;
+
+        int Q = 0;
+
+        ts::sink8_t s = ts::sink8_t(i & 0xff);
+        while (Q < 8) {
+            ts::sink8_t ts = ts::to_sink(ts::to_float(s, Q), Q);
+            ++count;
+            if (ts == s) {
+                ++match;
+            } else {
+                std::cerr << "Missmatch sink(Q=" << Q <<"): 0x" << std::hex << uint32_t(s) << " vs. 0x" << uint32_t(ts)<< std::oct << std::endl;
+            }
+
+            ++Q;
+        }
+
+        ++i;
+    }
+
+    std::cout << "count=" << count << ", match=" << match << std::endl;
+}
+
+
+int main() {
+    // plot_list();
+
+    std::cout << "Converting each sink8 to float then convert back." << std::endl;
+
+    test();
+
+    return 0;
+}
