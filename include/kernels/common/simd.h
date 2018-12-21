abb16a857ee7ad3cb3605cbe07c6817eb245af46
kaizhou.li <kaizhou.li@seetatech.com>
2018-12-22 03:08:48 +0800

    add simd.h for simd coding

---
+++ include/kernels/common/simd.h
@@ -0,0 +1,88 @@
+//
+// Created by kier on 2018/12/21.
+//
+
+#ifndef TENSORSTACK_KERNELS_COMMON_SIMD_H
+#define TENSORSTACK_KERNELS_COMMON_SIMD_H
+
+#include "simd_def.h"
+
+namespace ts {
+    template <typename T, int M>
+    class simd_base {
+    public:
+        using self = simd_base;
+        using base = T;
+        static const int width = M;
+    };
+
+    template <typename T, int M>
+    class simd : public simd_base<T, M> {
+    public:
+        using self = simd;
+        using supper = simd_base<T, M>;
+
+        void store(typename supper::base *p) const;
+    };
+
+    using float32x4 = simd<float, 4>;
+
+    template <typename T, int M>
+    inline T sum(simd<T, M> value) {
+        T a[M];
+        value.store(a);
+        T sum = 0;
+        for (int i = 0; i < M; ++i) sum += a[i];
+        return sum;
+    }
+
+    template <typename T>
+    inline T sum(simd<T, 4> value) {
+        T a[4];
+        value.store(a);
+        return a[0] + a[1] + a[2] + a[3];
+    }
+
+    template <>
+    class simd<float, 4> : public simd_base<float, 4> {
+    public:
+        using self = simd;
+        using type = _simd_f32x4;
+
+        type value;
+
+        simd() = default;
+        simd(type value) : value(value) {}
+        simd(base a) : simd(a, a, a, a) {}
+
+        simd(const base *p);
+
+        simd(base a, base b, base c, base d);
+
+        void store(base *p) const;
+    };
+
+    simd<float, 4>::simd(const base *p) : value(_simd_f32x4_load(p)) {}
+
+    simd<float, 4>::simd(base a, base b, base c, base d) : value(_simd_f32x4_set(a, b, c, d)) {}
+
+    void simd<float, 4>::store(base *p) const { _simd_f32x4_store(p, value); }
+
+    inline simd<float, 4> operator+(const simd<float, 4> &lhs, const simd<float, 4> &rhs) {
+        return _simd_f32x4_add(lhs.value, rhs.value);
+    }
+
+    inline simd<float, 4> operator-(const simd<float, 4> &lhs, const simd<float, 4> &rhs) {
+        return _simd_f32x4_sub(lhs.value, rhs.value);
+    }
+
+    inline simd<float, 4> operator*(const simd<float, 4> &lhs, const simd<float, 4> &rhs) {
+        return _simd_f32x4_mul(lhs.value, rhs.value);
+    }
+
+    inline simd<float, 4> operator/(const simd<float, 4> &lhs, const simd<float, 4> &rhs) {
+        return _simd_f32x4_div(lhs.value, rhs.value);
+    }
+}
+
+#endif //TENSORSTACK_KERNELS_COMMON_SIMD_H
