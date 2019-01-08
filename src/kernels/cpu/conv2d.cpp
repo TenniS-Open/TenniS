3e4f1045a1aab87731e19ea8f5d750cbd5517c1c
wangquyuan <quyuan.wang@seetatech.com>
2019-01-08 17:58:38 +0800

    add conv2d,shape_operator,pad

---
+++ src/kernels/cpu/conv2d.cpp
@@ -0,0 +1,7 @@
+#include <kernels/cpu/conv2d.h>
+
+namespace ts {
+
+TS_REGISTER_OPERATOR(Conv2d, ts::CPU, "conv2d")
+
+}
---
+++ src/kernels/cpu/pad.cpp
@@ -0,0 +1,7 @@
+#include <kernels/cpu/pad.h>
+
+namespace ts {
+
+TS_REGISTER_OPERATOR(Pad, ts::CPU, "pad")
+
+}
