1fd59805bdc721831f6664994d0266ebd7ec6619
wangquyuan <quyuan.wang@seetatech.com>
2019-01-09 18:15:48 +0800

    add by wqy

--- src/kernels/cpu/dimshuffle.cpp
+++ src/kernels/cpu/dimshuffle.cpp
@@ -1,6 +1,126 @@
 #include <kernels/cpu/dimshuffle.h>
+#include <core/tensor_builder.h>
+
 
 namespace ts {
+
+//////////////////////////////////////////
+Dimshuffle::Dimshuffle() {
+    field("dim", REQUIRED);
+    field("shuffle", REQUIRED);
+    m_dim = -1;
+}
+
+void Dimshuffle::init() {
+    supper::init();
+ 
+    if(!has("dim")){
+        throw ts::Exception("Dimshuffle input parameter dim do not find");
+    }
+
+    if(!has("shuffle")){
+        throw ts::Exception("Dimshuffle input parameter shuffle do not find");
+    }
+
+    Tensor tensor_dim = get("dim");
+    m_dim = ts::tensor::to_int(tensor_dim);
+
+    Tensor tensor_shuffle = get("shuffle");
+    if(tensor_shuffle.dtype() != ts::INT32) {
+        throw ts::Exception("Dimshuffle input parameter shuffle only support INT32 type");
+    }
+    m_shuffle.resize(tensor_shuffle.count());
+    for(int i=0; i<tensor_shuffle.count(); i++) {
+        m_shuffle[i] = tensor_shuffle.sync(memory_device()).data<int>()[i];
+    }
+}
+
+void Dimshuffle::infer_private(ts::Stack &stack, ts::Tensor::Prototype &output) {
+    int input_num = stack.size();
+    if(input_num != 1) {
+        throw ts::Exception("Dimshuffle input parameters is more than one");
+    }
+
+    Shape shape = stack.index(0)->sizes();
+
+    if(shape.size()  < 1) {
+        throw ts::Exception("Dimshuffle input parameters dims is litter 1");
+    }
+
+    if(m_dim < 0 || m_dim >= shape.size()) {
+        throw ts::Exception("Dimshuffle input parameter dim value check failed");
+    }
+
+    for(int i=0; i<m_shuffle.size(); i++) {
+        if(m_shuffle[i] < 0 || m_shuffle[i] >= shape[m_dim]) {
+            throw ts::Exception("Dimshuffle shuffle parameter is invalid");
+        }
+    }
+
+    if(m_shuffle.size() != shape[m_dim]) {
+        shape[m_dim] = m_shuffle.size();
+    }
+    output = ts::Tensor::Prototype(stack.index(0)->dtype(), shape);
+}
+
+
+
+int Dimshuffle::infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output) {
+    output.resize(1);
+    infer_private(stack, output[0]);
+    return 1;
+}
+
+int Dimshuffle::run(ts::Stack &stack) {
+    ts::Tensor::Prototype output;
+    infer_private(stack, output);
+
+    ts::Tensor *input_tensor = stack.index(0);
+    Shape shape = input_tensor->sizes();
+    Shape reshape = output.sizes();
+
+    int type_len = ts::type_bytes(input_tensor->dtype());
+
+    stack.push(output, memory_device());
+    ts::Tensor *tensor = stack.index(-1);
+
+    unsigned int preoffset = 1;
+    for(int i=0; i<m_dim; i++) {
+        preoffset *= shape[i];
+    }
+
+    unsigned int stride = 1;
+    for(int i=m_dim; i<shape.size(); i++) {
+        stride *= shape[i];
+    }
+
+    unsigned int backstride = 1;
+    backstride = stride / shape[m_dim];
+    unsigned int newstride = backstride * m_shuffle.size();
+
+    for(int k=0; k<preoffset; k++) {
+        for(int i=0; i<m_shuffle.size(); i++) {
+            ::memcpy(tensor->sync(memory_device()).data() + type_len * (k * newstride + i * backstride),
+                     input_tensor->sync(memory_device()).data() + type_len * (k * stride + m_shuffle[i] * backstride),
+                     backstride * type_len);
+        }
+    }
+
+    return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////
 
 TS_REGISTER_OPERATOR(Dimshuffle, ts::CPU, "_dimshuffle")
 
--- src/kernels/cpu/image_resize2d.cpp
+++ src/kernels/cpu/image_resize2d.cpp
@@ -1,5 +1,7 @@
 #include <kernels/cpu/image_resize2d.h>
 #include <kernels/cpu/saturate_cast.h>
+
+#include <core/tensor_builder.h>
 #include <memory>
 
 namespace ts {
@@ -191,6 +193,176 @@ template<typename T>
     }
 }
 
+
+///////////////////////////////////////////////////////////////
+Image_Resize2d:: Image_Resize2d() {
+    field("type", OPTIONAL);
+    m_type = 0;
+}
+
+void Image_Resize2d::init() {
+    supper::init();
+
+    if(has("type")){
+        m_type = ts::tensor::to_int(get("type"));
+    }
+}
+
+
+int Image_Resize2d::infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output) {
+    if(stack.size() != 2 || stack.index(0)->dims() < 2 || stack.index(0)->dims() != stack.index(1)->count()) {
+        throw ts::Exception("input parameters is invalid");
+    }
+
+    int type_len = ts::type_bytes(stack.index(0)->dtype());
+
+    if(stack.index(1)->dtype() != ts::INT32) {
+        throw ts::Exception("input parameters 1 only support INT32 type");
+    }
+
+    int dims = stack.index(0)->dims();
+    int i=0;
+    for(i=0; i< dims; i++) {
+         if((int)(stack.index(1)->sync(memory_device()).data<int>()[i]) > 0)
+            break;
+    }
+
+    if(i >= dims - 1) {
+        throw ts::Exception("input parameters dims invalid");
+    }
+
+    int new_height = (int)stack.index(1)->sync(memory_device()).data<int>()[i];
+    int new_width  = (int)stack.index(1)->sync(memory_device()).data<int>()[i+1];
+    //int old_height = (int)stack.index(0)->sizes()[i];
+    //int old_width  = (int)stack.index(0)->sizes()[i+1];
+
+    ts::Shape shape(stack.index(0)->sizes());
+
+    shape[i] = new_height;
+    shape[i+1] = new_width;
+
+    output.resize(1);
+    output[0] = ts::Tensor::Prototype(stack.index(0)->dtype(),shape);
+    return 1;
+}
+
+
+template<typename T>
+void Image_Resize2d::resize(ts::Stack &stack, ts::Tensor &tensor, int old_height, int old_width,
+           int new_height,int new_width, unsigned int oldstep, unsigned int newstep, unsigned int step, int channels, bool ishwc) {
+
+    T*  psrc = stack.index(0)->sync(memory_device()).data<T>() + oldstep;;
+    T*  pdst = tensor.sync(memory_device()).data<T>() + newstep;;
+
+
+    if(m_type == 0) {
+        ResizeImageLinear(psrc, old_width, old_height, channels, pdst,new_width,new_height, ishwc);
+    }else {
+        ResizeImageCubic(psrc, old_width, old_height, channels, pdst,new_width,new_height, ishwc);
+    }
+    //::cv::Mat srcimage(old_height, old_width,imagetype,psrc);
+    ////imwrite("/tmp/kkk3.png",srcimage);
+    //::cv::Mat dstimage;// = srcimage;
+    //::cv::resize(srcimage, dstimage, cv::Size(new_height, new_width),0,0, type);
+    //::memcpy(pdst, dstimage.data, step * sizeof(T));
+}
+
+int Image_Resize2d::run(ts::Stack &stack) {
+    int input_num = stack.size();
+
+    if(input_num != 2 || stack.index(0)->dims() < 2 || stack.index(0)->dims() != stack.index(1)->count()) {
+        throw ts::Exception("input parameters is invalid");
+    }
+
+    int type_len = ts::type_bytes(stack.index(0)->dtype());
+
+    if(stack.index(1)->dtype() != ts::INT32) {
+        throw ts::Exception("input parameters 1 only support INT32 type");
+    }
+    int dims = stack.index(0)->dims();
+    int i=0;
+    for(i=0; i< dims; i++) {
+        if((int)(stack.index(1)->data<int>()[i]) > 0)
+            break;
+    }
+
+    if(i >= dims - 1) {
+        throw ts::Exception("input parameters dims invalid");
+    }
+
+    int new_height = (int)stack.index(1)->sync(memory_device()).data<int>()[i];
+    int new_width  = (int)stack.index(1)->sync(memory_device()).data<int>()[i+1];
+    int old_height = (int)stack.index(0)->sizes()[i];
+    int old_width  = (int)stack.index(0)->sizes()[i+1];
+
+    std::vector<ts::Tensor::Prototype> output;
+    infer(stack, output);
+
+    ts::Shape shape(output[0].sizes());
+    //ts::Shape shape(stack.index(0)->sizes());
+    //shape[i] = new_height;
+    //shape[i+1] = new_width;
+
+    int ntotalbuffer = 0;
+    int batchs,channels;
+    batchs = channels = 0;
+    bool ishwc = true;
+    if(dims == 2) {
+        batchs = channels = 1;
+    }else if(dims == 3) {
+       if(i== 0) {
+           batchs = 1;
+           channels = shape[2];
+       }else if(i==1) {
+           batchs = shape[0];
+           channels = 1;
+       }
+
+    }else if(dims == 4) {
+       if(i==0) {
+           throw ts::Exception("input parameters dims invalid");
+       }else if(i==1) {
+          batchs = shape[0];
+          channels = shape[3];
+       }else if(i==2) {
+          batchs = shape[0];
+          channels = shape[1];
+          ishwc = false;
+       }
+    }
+
+    if(batchs <= 0 || channels <= 0) {
+        throw ts::Exception("input parameters dims invalid");
+    }
+
+    stack.push(output[0], memory_device());
+
+    int newstep = channels * new_height * new_width;
+    int oldstep = channels * old_height * old_width;
+
+    auto &tensor = *stack.index(-1);
+
+    for(int k=0; k<batchs; k++) {
+        if(type_len == 1){
+            resize<unsigned char>(stack, tensor, old_height,old_width,new_height,new_width,
+                                   k * oldstep, k * newstep, newstep, channels, ishwc);
+        }else if(type_len == 2) {
+            resize<unsigned short>(stack, tensor, old_height,old_width,new_height,new_width,
+                                     k * oldstep, k * newstep,  newstep, channels, ishwc);
+        }else if(type_len == 4) {
+            resize<float>(stack, tensor, old_height,old_width,new_height,new_width,
+                                     k * oldstep, k * newstep, newstep, channels, ishwc);
+        }else if(type_len == 8) {
+            resize<double>(stack, tensor, old_height,old_width,new_height,new_width,
+                                     k * oldstep, k * newstep, newstep, channels, ishwc);
+        }
+
+    }
+
+    return 1;
+}
+
+////////////////////////////////////////////////////////////////
 
 
 TS_REGISTER_OPERATOR(Image_Resize2d, ts::CPU, "_image_resize2d")
