516d69e37ec0d8ca6b809d3d59a8d21c6736d25e
chaoyang <chao.yang@seetatech.com>
2019-01-10 19:32:47 +0800

    add relu operator

---
+++ include/kernels/cpu/relu.h
@@ -0,0 +1,27 @@
+#ifndef TS_KERNELS_OP_RELU_H
+#define TS_KERNELS_OP_RELU_H
+
+#include <global/operator_factory.h>
+#include <core/tensor.h>
+#include <runtime/stack.h>
+
+namespace ts {
+	class Relu : public ts::Operator {
+	public:
+		using supper = ts::Operator;
+		Relu() {}
+		virtual void init();
+		virtual int run(ts::Stack &stack);
+		virtual int infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output);
+
+	private:
+		template<typename T>
+		bool relu(ts::Stack &stack);
+	};
+
+	TS_REGISTER_OPERATOR(Relu, ts::CPU, "relu")
+}
+
+
+
+#endif //TS_KERNELS_OP_RELU_H
