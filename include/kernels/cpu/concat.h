857fe828523d07a4538a91962a0a886edb0d7be2
chaoyang <chao.yang@seetatech.com>
2019-01-10 15:30:32 +0800

    add concat operator

---
+++ include/kernels/cpu/concat.h
@@ -0,0 +1,32 @@
+#ifndef TS_KERNELS_CONCAT_H
+#define TS_KERNELS_CONCAT_H
+
+#include <global/operator_factory.h>
+#include <core/tensor.h>
+#include <runtime/stack.h>
+
+namespace ts {
+
+	class Concat : public ts::Operator {
+	public:
+		using supper = ts::Operator;
+		Concat() {
+			field("dim", REQUIRED);
+		}
+		virtual void init();
+		virtual int run(ts::Stack &stack);
+		virtual int infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output);
+	private:
+
+		template<typename T>
+		bool concat(ts::Stack &stack, int input_num);
+
+	private:
+		int m_dim;
+	};
+
+	TS_REGISTER_OPERATOR(Concat, ts::CPU, "concat")
+}
+
+
+#endif //TS_KERNELS_CONCAT_H
