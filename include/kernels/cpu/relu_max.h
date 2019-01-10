cb57f3f71b706da573f9a43fb7e4a3b223c22beb
chaoyang <chao.yang@seetatech.com>
2019-01-10 20:20:41 +0800

    add relu_max operator

---
+++ include/kernels/cpu/relu_max.h
@@ -0,0 +1,32 @@
+#ifndef TS_KERNELS_RELUMAX_H
+#define TS_KERNELS_RELUMAX_H
+
+#include <global/operator_factory.h>
+#include <core/tensor.h>
+#include <runtime/stack.h>
+
+namespace ts {
+	class ReluMax : public ts::Operator {
+	public:
+		using supper = ts::Operator;
+		ReluMax() {
+			field("max", REQUIRED);
+		}
+		virtual void init();
+		virtual int run(ts::Stack &stack);
+		virtual int infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output);
+
+	private:
+		template<typename T>
+		bool relu_max(ts::Stack &stack);
+
+	private:
+		float m_max;
+	};
+
+	TS_REGISTER_OPERATOR(ReluMax, ts::CPU, "relu_max")
+}
+
+
+
+#endif //TS_KERNELS_RELUMAX_H
