8290a060b582c44417bd8c310c565792d95143b0
kaizhou.li <kaizhou.li@seetatech.com>
2019-01-19 11:01:18 +0800

    add test code to test dir

--- python/test/module.py
+++ python/test/module.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
-from tensorstack import Node
+import os
+import sys
+sys.path.append(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)),".."))
+
 from tensorstack import Module
 from tensorstack import bubble
 from tensorstack import tensor
 from tensorstack import device
-from tensorstack import zoo
 
 
-if __name__ == '__main__':
+def test():
     a = bubble.param("a")
     b = bubble.param("b")
-    data = bubble.data("data", tensor.from_any(3, dtype=float), device=device.CPU)\
+    data = bubble.data("data", tensor.from_any(3, dtype=float), device=device.CPU)
 
     c = bubble.op("c", "sum", [a, b, data])
 
@@ -19,5 +22,9 @@ if __name__ == '__main__':
 
     module.load(c)
 
-    with open("test.module.txt", "w") as fo:
+    with open("test.module.txt", "wb") as fo:
         Module.Save(stream=fo, module=module)
+
+
+if __name__ == '__main__':
+    test()
