c6b0d7342e42e43780471612575309a8688b9b5f
wangquyuan <quyuan.wang@seetatech.com>
2019-01-24 16:40:51 +0800

    modify conv2d im2col

--- include/kernels/cpu/im2col.h
+++ include/kernels/cpu/im2col.h
@@ -14,7 +14,7 @@ void im2col_nd_cpu(const Dtype* data_im, const int num_spatial_axes,
 template <typename Dtype>
 void im2col_cpu(const Dtype* data_im, const int channels,
     const int height, const int width, const int kernel_h, const int kernel_w,
-    const int pad_h, const int pad_w, const int stride_h,
+    const int pad_h_top,const int pad_h_bottom, const int pad_w_left,const int pad_w_right, const int stride_h,
     const int stride_w, const int dilation_h, const int dilation_w,
     Dtype* data_col, const int padding_value);
 
