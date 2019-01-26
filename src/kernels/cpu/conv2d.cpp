4589e0bade0f554576b9bce5368e7c4ce04e7b4b
wangquyuan <quyuan.wang@seetatech.com>
2019-01-26 14:04:13 +0800

    add cblas's sgemm and dgemm

--- src/kernels/cpu/conv2d.cpp
+++ src/kernels/cpu/conv2d.cpp
@@ -6,6 +6,11 @@
 #include <backend/name.h>
 #include <core/device.h>
 #include <utils/assert.h>
+#ifdef TS_USE_CBLAS
+extern "C" {
+#include <cblas.h>
+}
+#endif
 
 namespace ts {
 
@@ -65,6 +70,7 @@ int Conv2d::infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output) 
     return infer_private(stack, output[0]);
 }
 
+
 template<typename T>
 void Conv2d::compute_conv(Tensor *input_tensor, Tensor *weight_tensor, Tensor *tensor, const Shape& shape,
                       const Shape &reshape, const Shape &weight_shape ) {
@@ -81,7 +87,7 @@ void Conv2d::compute_conv(Tensor *input_tensor, Tensor *weight_tensor, Tensor *t
     col_shape.resize(1);
     col_shape[0] = col_buffer_size;
     Tensor col_tensor(MemoryDevice(CPU), input_tensor->dtype(), col_shape); 
-    T * col_buffer = col_tensor.data<T>();//new T [col_buffer_size];
+    T * col_buffer = col_tensor.data<T>();
 
     const T *pinput = input_tensor->sync(MemoryDevice(CPU)).data<T>();
     const T *pweight = weight_tensor->sync(MemoryDevice(CPU)).data<T>();
@@ -97,11 +103,86 @@ void Conv2d::compute_conv(Tensor *input_tensor, Tensor *weight_tensor, Tensor *t
         pinput += input_number_offset;
         poutput+= output_number_offset;
     }
-
-    //delete [] col_buffer;
-
 }
 
+
+template<>
+void Conv2d::compute_conv<float>(Tensor *input_tensor, Tensor *weight_tensor, Tensor *tensor, const Shape& shape,
+                      const Shape &reshape, const Shape &weight_shape ) {
+
+    int kernel_dims = weight_shape[1] * weight_shape[2] * weight_shape[3];
+    int conv_out_spatial_dim = reshape[2] * reshape[3];
+    //int col_offset = kernel_dims * conv_out_spatial_dim;
+    //int weight_offset = weight_shape[0] * kernel_dims;
+    int output_number_offset = reshape[1] * conv_out_spatial_dim;
+    int input_number_offset = shape[1] * shape[2] * shape[3];
+    int col_buffer_size = shape[1] * weight_shape[2] * weight_shape[3] * reshape[2] * reshape[3];
+
+    Shape col_shape;
+    col_shape.resize(1);
+    col_shape[0] = col_buffer_size;
+    Tensor col_tensor(MemoryDevice(CPU), input_tensor->dtype(), col_shape); 
+    float * col_buffer = col_tensor.data<float>();
+
+    const float *pinput = input_tensor->sync(MemoryDevice(CPU)).data<float>();
+    const float *pweight = weight_tensor->sync(MemoryDevice(CPU)).data<float>();
+    float *poutput = tensor->data<float>();
+    for(int i=0; i<shape[0]; i++) {
+        ::memset(col_buffer, 0, col_buffer_size * sizeof(float));
+        im2col_cpu(pinput, shape[1], shape[2], shape[3], weight_shape[2], weight_shape[3],
+                   m_padding[4], m_padding[5],m_padding[6],m_padding[7], m_stride[2], m_stride[3],m_dialations[2],m_dialations[3], col_buffer, m_padding_value);
+
+#ifdef TS_USE_CBLAS
+        std::cout << "use open cblas float" << std::endl; 
+        cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, weight_shape[0], conv_out_spatial_dim,
+                    kernel_dims, (float)1, pweight, kernel_dims, col_buffer, conv_out_spatial_dim, (float)0,  poutput, conv_out_spatial_dim);
+#else
+        cpu::math<float>::gemm(ts::blas::NoTrans,ts::blas::NoTrans, weight_shape[0], conv_out_spatial_dim,
+                               kernel_dims, (float)1, pweight, col_buffer, (float)0, poutput);
+#endif
+        pinput += input_number_offset;
+        poutput+= output_number_offset;
+    }
+}
+
+template<>
+void Conv2d::compute_conv<double>(Tensor *input_tensor, Tensor *weight_tensor, Tensor *tensor, const Shape& shape,
+                      const Shape &reshape, const Shape &weight_shape ) {
+
+    int kernel_dims = weight_shape[1] * weight_shape[2] * weight_shape[3];
+    int conv_out_spatial_dim = reshape[2] * reshape[3];
+    //int col_offset = kernel_dims * conv_out_spatial_dim;
+    //int weight_offset = weight_shape[0] * kernel_dims;
+    int output_number_offset = reshape[1] * conv_out_spatial_dim;
+    int input_number_offset = shape[1] * shape[2] * shape[3];
+    int col_buffer_size = shape[1] * weight_shape[2] * weight_shape[3] * reshape[2] * reshape[3];
+
+    Shape col_shape;
+    col_shape.resize(1);
+    col_shape[0] = col_buffer_size;
+    Tensor col_tensor(MemoryDevice(CPU), input_tensor->dtype(), col_shape); 
+    double * col_buffer = col_tensor.data<double>();
+
+    const double *pinput = input_tensor->sync(MemoryDevice(CPU)).data<double>();
+    const double *pweight = weight_tensor->sync(MemoryDevice(CPU)).data<double>();
+    double *poutput = tensor->data<double>();
+    for(int i=0; i<shape[0]; i++) {
+        ::memset(col_buffer, 0, col_buffer_size * sizeof(double));
+        im2col_cpu(pinput, shape[1], shape[2], shape[3], weight_shape[2], weight_shape[3],
+                   m_padding[4], m_padding[5],m_padding[6],m_padding[7], m_stride[2], m_stride[3],m_dialations[2],m_dialations[3], col_buffer, m_padding_value);
+
+#ifdef TS_USE_CBLAS
+        std::cout << "use open cblas double" << std::endl; 
+        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, weight_shape[0], conv_out_spatial_dim,
+                    kernel_dims, (double)1, pweight, kernel_dims, col_buffer, conv_out_spatial_dim, (double)0,  poutput, conv_out_spatial_dim);
+#else
+        cpu::math<double>::gemm(ts::blas::NoTrans,ts::blas::NoTrans, weight_shape[0], conv_out_spatial_dim,
+                               kernel_dims, (double)1, pweight, col_buffer, (double)0, poutput);
+#endif
+        pinput += input_number_offset;
+        poutput+= output_number_offset;
+    }
+}
 
 int Conv2d::run(ts::Stack &stack) {
     Tensor::Prototype output;
