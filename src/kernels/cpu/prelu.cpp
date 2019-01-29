13a4320ad080d8a9f7a6ffa2607bbec7140d0ef3
chaoyang <chao.yang@seetatech.com>
2019-01-29 14:58:27 +0800

    fix bug about switch bug

--- src/kernels/cpu/prelu.cpp
+++ src/kernels/cpu/prelu.cpp
@@ -43,30 +43,25 @@ namespace ts {
 		stack.push(output[0], memory_device());
 
 		auto dtype = stack.index(0)->dtype();
-		int bytes = type_bytes(dtype);
-		bool flag;
-		switch (bytes)
-		{
-			case 1: flag = prelu<char>(stack); break;
-			case 2: flag = prelu<short>(stack); break;
-			case 4 : flag = prelu<float>(stack); break;
-			case 8: flag = prelu<double>(stack); break;
-			default:break;
+		switch (dtype) {
+	#define DECLARE_TYPE_AND_RUN(DTYPE, TYPE) \
+					case DTYPE: { prelu<TYPE>(stack); break; }
+				DECLARE_TYPE_AND_RUN(INT8, int8_t);
+				DECLARE_TYPE_AND_RUN(UINT8, uint8_t);
+				DECLARE_TYPE_AND_RUN(INT16, int16_t);
+				DECLARE_TYPE_AND_RUN(UINT16, uint16_t);
+				DECLARE_TYPE_AND_RUN(INT32, int32_t);
+				DECLARE_TYPE_AND_RUN(UINT32, uint32_t);
+				DECLARE_TYPE_AND_RUN(INT64, int64_t);
+				DECLARE_TYPE_AND_RUN(UINT64, uint64_t);
+				DECLARE_TYPE_AND_RUN(FLOAT32, float);
+				DECLARE_TYPE_AND_RUN(FLOAT64, double);
+	#undef DECLARE_TYPE_AND_RUN
+			default: {
+				TS_LOG_ERROR << "prelu not support this data type: " << dtype << eject;
+				break;
+			}
 		}
-		//switch (dtype)
-		//{
-		//	case ts::FLOAT32:
-		//	{
-		//		flag = prelu<float>(stack);
-		//		break;
-		//	}
-		//	case ts::FLOAT64:
-		//	{
-		//		flag = prelu<double>(stack);
-		//		break;
-		//	}
-		//	default:break;
-		//}
 		return 1;
 	}
 
