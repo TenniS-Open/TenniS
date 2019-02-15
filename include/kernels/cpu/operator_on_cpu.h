955fc317f803296abeecebefbb57d3c4e83a2308
kaizhou.li <kaizhou.li@seetatech.com>
2019-02-15 11:11:18 +0800

    update base

---
+++ include/kernels/cpu/operator_on_cpu.h
@@ -0,0 +1,23 @@
+//
+// Created by kier on 2019/2/15.
+//
+
+#ifndef TENSORSTACK_KERNELS_CPU_OPERATOR_ON_CPU_H
+#define TENSORSTACK_KERNELS_CPU_OPERATOR_ON_CPU_H
+
+#include "backend/base/operator_on_device.h"
+
+namespace ts {
+    namespace cpu {
+        template<typename OP>
+        class OperatorOnCPU : public OP {
+        public:
+            virtual MemoryDevice running_memory_device() {
+                return MemoryDevice(CPU);
+            }
+        };
+    }
+}
+
+
+#endif //TENSORSTACK_KERNELS_CPU_OPERATOR_ON_CPU_H
