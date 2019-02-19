2065a90e39f00cd37956a599e82a5b8bf6411a2a
kaizhou.li <kaizhou.li@seetatech.com>
2019-02-19 19:57:23 +0800

    handly reversion code, waiting bug fix

--- include/kernels/cpu/pooling2d.h
+++ include/kernels/cpu/pooling2d.h
@@ -1,16 +1,54 @@
-#ifndef TENSORSTACK_KERNELS_CPU_POOLING2D_H
-#define TENSORSTACK_KERNELS_CPU_POOLING2D_H
+#ifndef TS_KERNELS_POOLONG2D_H
+#define TS_KERNELS_POOLONG2D_H
 
-#include "operator_on_cpu.h"
-#include "backend/base/base_pooling2d.h"
-#include "pooling2d_core.h"
+#include <global/operator_factory.h>
+#include <core/tensor.h>
+#include <runtime/stack.h>
 
+#include "backend/common_structure.h"
 
 namespace ts {
-	namespace cpu {
-	    using Pooling2D = base::Pooling2DWithCore<OperatorOnCPU<base::Pooling2D>, Pooling2DCore>;
-	}
+	class Pooling2d : public ts::Operator {
+	public:
+		using supper = ts::Operator;
+		Pooling2d();
+
+		enum POOLINGTYPE {
+			max,
+			avg
+		};
+
+		enum PDDINGTYPE {
+			black,
+			copy,
+			loop
+		};
+
+		virtual void init();
+		virtual int run(ts::Stack &stack);
+		virtual int infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output);
+
+	private:
+
+		template<typename T>
+		bool pooling(ts::Stack &stack);
+
+		template<typename T>
+		bool max_pooling(T* input_data, T* output_data, Shape& input_shape, Shape& output_shape, KSize2D& ksize, Stride2D& stride);
+
+		template<typename T>
+		bool average_pooling(T* input_data, T* output_data, Shape& input_shape, Shape& output_shap, KSize2D& ksize, Stride2D& stride);
+
+	private:
+
+		Padding2D m_padding;
+		std::string m_format;
+		POOLINGTYPE m_pooling_type;
+		PDDINGTYPE m_padding_type;
+		KSize2D m_ksize;
+		Stride2D m_stride;
+	};
+	//TS_REGISTER_OPERATOR(Pooling2d, ts::CPU, "pooling2d")
 }
 
-
-#endif //TENSORSTACK_KERNELS_CPU_POOLING2D_H
+#endif //TS_KERNELS_POOLONG2D_H
