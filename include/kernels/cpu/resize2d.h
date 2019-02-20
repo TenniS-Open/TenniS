90a6e7b1e5f43e67770252e24fb482e6651bc3ca
kaizhou.li <kaizhou.li@seetatech.com>
2019-02-20 22:41:06 +0800

    update base of resize2d

--- include/kernels/cpu/resize2d.h
+++ include/kernels/cpu/resize2d.h
@@ -1,55 +1,16 @@
-#ifndef TS_KERNELS_RESIZE2D_H
-#define TS_KERNELS_RESIZE2D_H
+#ifndef TENSORSTACK_KERNELS_CPU_RESIZE2D_H
+#define TENSORSTACK_KERNELS_CPU_RESIZE2D_H
 
-#include <core/tensor.h>
-#include <runtime/stack.h>
-#include <runtime/operator.h>
+#include "backend/base/base_resize2d.h"
+#include "operator_on_cpu.h"
 
 namespace ts {
-
-
-//support UINT8,UINT16,FLOAT32,FLOAT64, channel 1,2,3
-class Resize2d : public ts::Operator {
-public:
-
-
-    template<typename T>
-    static void ResizeImageLinear( const T *src_im, int src_width, int src_height, int channels,
-                                     T *dst_im, int dst_width, int dst_height);
-
-    template<typename T>
-    static void ResizeImageCubic(const T *src_im, int src_width, int src_height, int channels,
-                             T *dst_im, int dst_width, int dst_height);
-
-public:
-    enum RESIZETYPE{
-        linear,
-        cublic
-    };
-
-    using supper = ts::Operator;
-    Resize2d();
-
-    virtual void init();
-
-    virtual int run(ts::Stack &stack);
-
-    virtual int infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output); 
-
-private:
-template<typename T>
-    void resize(Tensor *input_tensor, Tensor *tensor, int old_height, int old_width, 
-           int new_height,int new_width, unsigned int oldstep, unsigned int newstep, 
-           unsigned int step, int channels); 
-
-private:
-    int m_type;
-};
-
-
-
-
-
+    namespace cpu {
+        class Resize2D : public OperatorOnCPU<base::Resize2D> {
+        public:
+            void resize2d(const Tensor &x, int dim, Resize2DType type, Tensor &out) override;
+        };
+    }
 }
 
-#endif
+#endif  // TENSORSTACK_KERNELS_CPU_RESIZE2D_H
---
+++ src/backend/base/base_resize2d.cpp
@@ -0,0 +1,101 @@
+//
+// Created by kier on 2019/2/20.
+//
+
+#include <backend/base/base_resize2d.h>
+
+#include "backend/base/base_resize2d.h"
+#include "backend/name.h"
+#include "core/tensor_builder.h"
+
+namespace ts {
+    namespace base {
+        Resize2D::Resize2D() {
+            field(name::type, OPTIONAL, tensor::from(int(Resize2DType::LINEAR)));
+        }
+
+        void Resize2D::init() {
+            m_type = Resize2DType(tensor::to_int(get(name::type)));
+        }
+
+        class _Resize2DTip {
+        public:
+            int dim;
+            int height;
+            int width;
+        };
+
+        int find_resized_height_dim(const int *size_data, int n) {
+            auto resized_height_dim = -1;
+            for (int i = 0; i < n; ++n) {
+                if (size_data[i] == 0) {
+                    return -1;
+                }
+                if (size_data[i] > 0) {
+                    resized_height_dim = i;
+                    break;
+                }
+            }
+            if (resized_height_dim < 0 ||
+                resized_height_dim >= n - 1 ||
+                size_data[resized_height_dim + 1] <= 0) {
+                return -1;
+            }
+            for (int i = resized_height_dim + 2; i < n; ++i) {
+                if (size_data[i] >= 0) {
+                    return -1;
+                }
+            }
+            return resized_height_dim;
+        }
+
+        static Tensor::Prototype check_outputs(const Stack &stack, int &dim) {
+            TS_AUTO_CHECK(stack.size() == 2);
+
+            auto &x = stack[0];
+            auto size = tensor::cast(INT32, stack[1]);
+
+            TS_AUTO_CHECK(size.dims() == 1 && x.dims() == size.size(0));
+            TS_AUTO_CHECK(size.size(0) >= 2);
+
+            auto n = size.size(0);
+            auto size_data = size.data<int32_t>();
+
+            auto resized_height_dim = find_resized_height_dim(size_data, n);
+            if (resized_height_dim < 0) {
+                TS_LOG_ERROR << "Can not resize " << to_string(x.sizes())
+                             << " to " << to_string(Shape(size_data, size_data + n)) << eject;
+            }
+
+            Shape resized_shape = x.sizes();
+            resized_shape[resized_height_dim] = size_data[resized_height_dim];
+            resized_shape[resized_height_dim + 1] = size_data[resized_height_dim + 1];
+
+            return Tensor::Prototype(x.dtype(), resized_shape);
+        }
+
+        int Resize2D::infer(Stack &stack, std::vector<Tensor::Prototype> &output) {
+            int dim;
+            auto resized_proto = check_outputs(stack, dim);
+
+            output.resize(1);
+            output[0] = resized_proto;
+
+            return 1;
+        }
+
+        int Resize2D::run(Stack &stack) {
+            int dim;
+            auto resized_proto = check_outputs(stack, dim);
+
+            auto memory_device = running_memory_device();
+
+            auto x = stack[0].view(memory_device);
+            auto out = *stack.push(resized_proto, memory_device);
+
+            resize2d(x, dim, m_type, out);
+
+            return 1;
+        }
+    }
+}
