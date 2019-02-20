c5010d530d027f025dd10a3c64e279f295c12885
kaizhou.li <kaizhou.li@seetatech.com>
2019-02-20 18:23:06 +0800

    update base of relu

--- src/kernels/cpu/relu.cpp
+++ src/kernels/cpu/relu.cpp
@@ -2,80 +2,52 @@
 #include <algorithm>
 
 #include "backend/name.h"
+#include "global/operator_factory.h"
 
 namespace ts {
+    namespace cpu {
+        template<typename T>
+        static void cpu_relu_compute_run(const Tensor &x, Tensor &out) {
+            const T *input_data = x.data<T>();
+            T *output_data = out.data<T>();
+            int count = out.count();
 
-	void Relu::init()
-	{
-		supper::init();
-	}
+            std::memcpy(output_data, input_data, count * sizeof(T));
 
-	int Relu::run(ts::Stack &stack)
-	{
-		std::vector<ts::Tensor::Prototype> output;
+            int counts = out.count();
+            for (int i = 0; i < counts; i++) {
+                T val = *output_data;
+                *output_data = std::max(val, T(0.0));
+                output_data++;
+            }
+        }
 
-		this->infer(stack, output);
-		stack.push(output[0], MemoryDevice(CPU));
-
-		auto dtype = stack.index(0)->dtype();
-		switch (dtype) {
-	#define DECLARE_TYPE_AND_RUN(DTYPE, TYPE) \
-				case DTYPE: { relu<TYPE>(stack); break; }
-				DECLARE_TYPE_AND_RUN(INT8, int8_t);
-				DECLARE_TYPE_AND_RUN(UINT8, uint8_t);
-				DECLARE_TYPE_AND_RUN(INT16, int16_t);
-				DECLARE_TYPE_AND_RUN(UINT16, uint16_t);
-				DECLARE_TYPE_AND_RUN(INT32, int32_t);
-				DECLARE_TYPE_AND_RUN(UINT32, uint32_t);
-				DECLARE_TYPE_AND_RUN(INT64, int64_t);
-				DECLARE_TYPE_AND_RUN(UINT64, uint64_t);
-				DECLARE_TYPE_AND_RUN(FLOAT32, float);
-				DECLARE_TYPE_AND_RUN(FLOAT64, double);
-	#undef DECLARE_TYPE_AND_RUN
-			default: {
-				TS_LOG_ERROR << "relu not support this data type: " << dtype << eject;
-				break;
-			}
-		}
-		return 1;
-	}
-
-	int Relu::infer(ts::Stack &stack, std::vector<ts::Tensor::Prototype> &output)
-	{
-		int input_num = stack.size();
-		TS_AUTO_CHECK(input_num == 1);
-
-		TS_AUTO_CHECK(stack.index(0)->dtype() == FLOAT32 || stack.index(0)->dtype() == FLOAT64);
-
-		output.resize(1);
-		output[0] = ts::Tensor::Prototype(stack.index(0)->dtype(), stack.index(0)->sizes());
-
-		return 1;
-	}
-
-	template<typename T>
-	bool Relu::relu(ts::Stack &stack)
-	{
-		ts::Tensor& output_tensor = *stack.index(-1);
-		auto output_shape = output_tensor.sizes();
-		auto input_memory = stack.index(0)->sync(MemoryDevice(CPU));
-		auto device_type = input_memory.device();
-		T* input_data = input_memory.data<T>();
-		T* output_data = output_tensor.data<T>();
-		int count = output_tensor.count();
-		memcpy(output_data, device_type, count * sizeof(T), input_data, device_type, count * sizeof(T));
-
-		int counts = output_tensor.count();
-		for (int i = 0; i < counts; i++)
-		{
-			T val = *output_data;
-			*output_data = std::max(val,T(0.0));
-			output_data++;
-		}
-
-		return true;
-	}
+        void ReLU::active(const Tensor &x, Tensor &out) {
+            // Notice: the all tensor' memory device are CPU, as given in running_memory_device
+            DTYPE dtype = out.dtype();
+            switch (dtype) {
+#define DECLARE_COMPUTE_RUN(DTYPE, TYPE) \
+        case DTYPE: { cpu_relu_compute_run<TYPE>(x, out); break; }
+                DECLARE_COMPUTE_RUN(INT8, int8_t);
+                DECLARE_COMPUTE_RUN(UINT8, uint8_t);
+                DECLARE_COMPUTE_RUN(INT16, int16_t);
+                DECLARE_COMPUTE_RUN(UINT16, uint16_t);
+                DECLARE_COMPUTE_RUN(INT32, int32_t);
+                DECLARE_COMPUTE_RUN(UINT32, uint32_t);
+                DECLARE_COMPUTE_RUN(INT64, int64_t);
+                DECLARE_COMPUTE_RUN(UINT64, uint64_t);
+                DECLARE_COMPUTE_RUN(FLOAT32, float);
+                DECLARE_COMPUTE_RUN(FLOAT64, double);
+#undef DECLARE_COMPUTE_RUN
+                default: {
+                    TS_LOG_ERROR << this->op() << " not support this data type: " << dtype << eject;
+                    break;
+                }
+            }
+        }
+    }
 }
 
 using namespace ts;
-TS_REGISTER_OPERATOR(Relu, ts::CPU, name::layer::relu())
+using namespace cpu;
+TS_REGISTER_OPERATOR(ReLU, ts::CPU, name::layer::relu())
