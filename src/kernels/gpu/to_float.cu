4a0782e9bca1c2abacf51a4e177db40b2ec61035
wangquyuan <quyuan.wang@seetatech.com>
2019-03-11 17:08:07 +0800

    add cast and to_float operator at gpu

---
+++ src/kernels/gpu/to_float.cu
@@ -0,0 +1,66 @@
+#include <kernels/gpu/to_float.h>
+#include <global/operator_factory.h>
+#include <backend/name.h>
+
+#include "utils/ctxmgr_lite.h"
+#include "core/device_context.h"
+
+#include "module/bubble.h"
+#include "core/tensor_builder.h"
+#include "utils/need.h"
+
+
+
+namespace ts {
+    namespace gpu {
+
+        ToFloat::ToFloat() {
+        }
+
+        void ToFloat::init() {
+            supper::init();
+
+            auto &context = ctx::ref<DeviceContext>();
+
+            m_op_castv2 = OperatorCreator::Create(context.computing_device.type(), name::layer::cast(), false);
+
+            TS_CHECK_NQ(m_op_castv2, nullptr) << "Can not find operator: " << name::layer::cast();
+
+            m_op_castv2->set(Bubble::RetentionParam::op, tensor::from(name::layer::conv2d_v2()));
+            m_op_castv2->set(Bubble::RetentionParam::name, tensor::from("_core" + name()));
+            m_op_castv2->set(Bubble::RetentionParam::output_count, get(Bubble::RetentionParam::output_count));
+
+            int dtype = FLOAT32;
+            m_op_castv2->set(name::dtype, tensor::from(dtype));
+
+        }
+
+
+        int ToFloat::infer(Stack &stack, std::vector<Tensor::Prototype> &output) {
+            TS_AUTO_CHECK(stack.size() == 1);
+
+            m_op_castv2->init();
+
+            stack.push(0);
+            return InferOperator(m_op_castv2, stack, 1, output);
+        }
+
+        int ToFloat::run(Stack &stack) {
+            TS_AUTO_CHECK(stack.size() == 1);
+
+            m_op_castv2->init();
+            stack.push(0);
+            return RunOperator(m_op_castv2, stack, 1);
+        }
+
+
+    }
+}
+
+
+
+
+using namespace ts;
+using namespace gpu;
+TS_REGISTER_OPERATOR(ToFloat, GPU, name::layer::to_float())
+
