ef79e85a6b594e1b35c90d4477b3e58ea5eca525
kaizhou.li <kaizhou.li@seetatech.com>
2019-03-16 10:54:47 +0800

    remove importing all sub modules

--- python/stackbuilder/__init__.py
+++ python/stackbuilder/__init__.py
@@ -4,9 +4,12 @@
 :author Kier
 """
 
-from . import holiday
-from . import onnx
-from . import torch
-from . import vvvv
+# from . import holiday
+# from . import onnx
+# from . import torch
+# from . import vvvv
+# TODO: the follow framework is comming
+# from . import tf
+# from . import mxnet
 
 __version__ = "0.0.9"
