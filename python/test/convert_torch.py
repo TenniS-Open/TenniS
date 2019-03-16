38d71e2bb7d97eb3eadce66ad640307d66ed1049
kaizhou.li <kaizhou.li@seetatech.com>
2019-03-16 10:56:09 +0800

    add torch converter

---
+++ python/test/convert_torch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+import sys
+sys.path.append(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+
+from stackbuilder.torch.converter import convert
+
+
+def test():
+    convert("cpu_checkpoint.pkl", "test.onnx.jit.tsm", (1, 3, 224, 224))
+
+
+if __name__ == '__main__':
+    test()
