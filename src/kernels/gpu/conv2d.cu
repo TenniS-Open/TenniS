110feb3403921643e67853d5520895b4557c71c7
chaoyang <chao.yang@seetatech.com>
2019-03-18 17:21:10 +0800

    modify regist operator

--- src/kernels/gpu/conv2d.cu
+++ src/kernels/gpu/conv2d.cu
@@ -4,8 +4,5 @@
 
 using namespace ts;
 using namespace gpu;
-#ifdef TS_USE_CUBLAS
 TS_REGISTER_OPERATOR(Conv2D, CUBLAS, name::layer::conv2d())
-#else
 TS_REGISTER_OPERATOR(Conv2D, GPU, name::layer::conv2d())
-#endif
