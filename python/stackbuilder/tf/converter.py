479db107884db8a5fc54ccb1814c3444e240fb26
kaizhou.li <kaizhou.li@seetatech.com>
2019-04-05 07:04:01 +0800

    base add structure of tf converter

--- python/stackbuilder/tf/converter.py
+++ python/stackbuilder/tf/converter.py
@@ -4,6 +4,80 @@
 author: kier
 """
 
+import tensorstack as ts
+
 
 def convert(graph, inputs, outputs, output_file):
+    if inputs is None:
+        raise Exception("param #2 inputs must be set.")
+    if outputs is None:
+        raise Exception("param #3 outputs must be set.")
+    if not isinstance(inputs, list) or not isinstance(inputs, tuple):
+        inputs = (inputs, )
+    if not isinstance(outputs, list) or not isinstance(outputs, tuple):
+        outputs = (outputs, )
+
+    # function format(node, inputs)
+    map_converter = {
+        # add layer converter here
+        "Identity": convert_identity,
+        "ConcatV2": convert_concat_v2,
+    }
+
+    map_tf_node_ts_node = {}
+
+    output_ts_nodes = []
+
+    def convert_node(tf_node):
+        if tf_node in map_tf_node_ts_node:
+            return map_tf_node_ts_node[tf_node]
+        node_op = tf_node.op
+        node_op_type = node_op.type
+        if node_op_type not in map_converter:
+            raise Exception("Not supported Layer: {}".format(node_op_type))
+        converter = map_converter[node_op_type]
+
+        input_ts_nodes = []
+        for input in node_op.inputs:
+            assert input != tf_node
+            # TODO: checking loop
+            input_ts_nodes.append(convert_node(input))
+
+        map_tf_node_ts_node[tf_node] = converter(tf_node, input_ts_nodes)
+
+    for output in outputs:
+        output_ts_nodes.append(convert_node(output))
+
+    print output_ts_nodes
+
+    # checking inputs
+    input_ts_nodes = []
+    for input in inputs:
+        if input not in map_tf_node_ts_node:
+            raise Exception("Node {} not in graph".format(input.op.name))
+        input_ts_nodes.append(map_tf_node_ts_node[input])
+
+    print input_ts_nodes
     pass
+
+
+def convert_identity(tf_node, inputs):
+    print("--# -=[ Converting {} layer: {} ]=-".format(tf_node.op.name, tf_node.op.type))
+    assert len(inputs) == 1
+    return inputs[0]
+
+
+def convert_concat_v2(tf_node, inputs):
+    print("--# -=[ Converting {} layer: {} ]=-".format(tf_node.op.name, tf_node.op.type))
+    assert len(inputs) > 0
+    N = tf_node.op.get_attr('N')
+
+    if N != len(inputs) - 1:
+        raise NotImplementedError("Concat N={} with {} inputs".format(N, len(inputs) - 1))
+
+    axis = tf_node.op.inputs[N].eval()
+
+    if axis < 0 or axis >= 4:
+        raise NotImplementedError("Concat axis: {}".format(axis))
+
+    return ts.zoo.concat(tf_node.op.name, inputs[:-1], dim=axis)
--- python/test/convert_tf.py
+++ python/test/convert_tf.py
@@ -13,20 +13,21 @@ import tensorflow as tf
 
 def test():
     graph = parser.loat_graph("tf/tensorflow.pb")
+    print [n.name for n in graph.as_graph_def().node]
 
     with graph.as_default():
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
-            inputs = sess.graph.get_tensor_by_name("input_image:0")
+            inputs = sess.graph.get_tensor_by_name("inputs_image:0")
             outputs = sess.graph.get_tensor_by_name("outputs:0")
 
             print inputs
             print outputs
 
             convert(graph,
-                    inputs=None,
-                    outputs=None,
+                    inputs=inputs,
+                    outputs=outputs,
                     output_file="tf.tsm")
 
 
