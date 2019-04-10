84dc2db557dddfe716c369eb0f52e2bc134a7783
chaoyang <chao.yang@seetatech.com>
2019-04-10 21:21:22 +0800

    support opt device

---
+++ include/kernels/opt/memory_opt.h
@@ -0,0 +1,14 @@
+#ifndef TENSORSTACK_KERNELS_OPT_MEMORY_OPT_H
+#define TENSORSTACK_KERNELS_OPT_MEMORY_OPT_H
+
+#include "global/hard_allocator.h"
+#include "global/hard_converter.h"
+
+namespace ts {
+    //void *cpu_allocator(int id, size_t new_size, void *mem, size_t mem_size);
+
+    //void cpu_converter(int dst_id, void *dst, int src_id, const void *src, size_t size);
+}
+
+
+#endif //TENSORSTACK_KERNELS_OPT_MEMORY_OPT_H
