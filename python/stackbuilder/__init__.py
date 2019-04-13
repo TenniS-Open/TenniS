61860f92e0234eb4129cb3bc2e9f6e5d91f884a7
kaizhou.li <kaizhou.li@seetatech.com>
2019-04-13 12:10:22 +0800

    add error message if module import failed

--- python/stackbuilder/__init__.py
+++ python/stackbuilder/__init__.py
@@ -7,30 +7,37 @@
 try:
     from . import holiday
 except Exception as e:
-    pass
+    import sys
+    sys.stderr.write("import holiday failed with: {}\n".format(e.message))
 try:
     from . import onnx
 except Exception as e:
-    pass
+    import sys
+    sys.stderr.write("import onnx failed with: {}\n".format(e.message))
 try:
     from . import torch
 except Exception as e:
-    pass
+    import sys
+    sys.stderr.write("import torch failed with: {}\n".format(e.message))
 try:
     from . import vvvv
 except Exception as e:
-    pass
+    import sys
+    sys.stderr.write("import vvvv failed with: {}\n".format(e.message))
 try:
     from . import caffe
 except Exception as e:
-    pass
+    import sys
+    sys.stderr.write("import caffe failed with: {}\n".format(e.message))
 try:
     from . import mxnet
 except Exception as e:
-    pass
+    import sys
+    sys.stderr.write("import mxnet failed with: {}\n".format(e.message))
 try:
     from . import tf
 except Exception as e:
-    pass
+    import sys
+    sys.stderr.write("import tensorflow failed with: {}\n".format(e.message))
 
 __version__ = "0.0.9"
