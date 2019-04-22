afb7d300a952975066de8a4afd385f6e5c095120
kaizhou.li <kaizhou.li@seetatech.com>
2019-04-22 11:25:12 +0800

    fix warning

--- src/kernels/gpu/batchtospace4d.cu
+++ src/kernels/gpu/batchtospace4d.cu
@@ -69,13 +69,13 @@ namespace ts {
             int input_number_step = input_channels * input_height * input_width;
             int input_channels_step = input_height * input_width;
             int input_height_step = input_width;
-            int input_width_step = 1;
+            // int input_width_step = 1;
 
             int output_size = output_number * output_channels * output_height * output_width;
             int output_number_step = output_channels * output_height * output_width;
             int output_channels_step = output_height * output_width;
             int output_height_step = output_width;
-            int output_width_step = 1;
+            // int output_width_step = 1;
 
             const T * pinput = x.data<T>();
             T * poutput = out.data<T>();
