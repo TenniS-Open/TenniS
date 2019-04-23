e41aeaa62ae38b82460021a136d589cef2daaf09
kaizhou.li <kaizhou.li@seetatech.com>
2019-04-23 16:40:34 +0800

    add white padding support

--- src/kernels/cpu/pooling2d_core.cpp
+++ src/kernels/cpu/pooling2d_core.cpp
@@ -112,19 +112,74 @@ namespace ts {
         }
 
         template<typename T>
+        static bool cpu_average_pooling_white(
+                const T *input_data, T *output_data,
+                const Shape &input_shape, const Shape &output_shape,
+                const KSize2D &ksize, const Stride2D &stride,
+                const Padding2D &m_padding) {
+            int input_h = input_shape[2];
+            int input_w = input_shape[3];
+            int output_h = output_shape[2];
+            int output_w = output_shape[3];
+            int input_channel_size = input_h * input_w;
+            int output_channel_size = output_h * output_w;
+            const int count = ksize.height * ksize.width;
+            for (int n = 0; n < output_shape[0]; n++) {
+                for (int c = 0; c < output_shape[1]; c++) {
+                    for (int oh = 0; oh < output_shape[2]; oh++) {
+                        int ihStart = oh * stride.height - m_padding.top;
+                        int ihEnd = std::min<T>(ihStart + ksize.height, input_h);
+                        for (int ow = 0; ow < output_shape[3]; ow++) {
+                            int iwStart = ow * stride.width - m_padding.left;
+                            int iwEnd = std::min<T>(iwStart + ksize.width, input_w);
+                            ihStart = std::max<T>(ihStart, 0);
+                            iwStart = std::max<T>(iwStart, 0);
+                            int outIndex = oh * output_w + ow;
+                            T sumValue = 0.0;
+                            for (int ih = ihStart; ih < ihEnd; ih++) {
+                                for (int iw = iwStart; iw < iwEnd; iw++) {
+                                    int input_index = ih * input_w + iw;
+                                    sumValue += input_data[input_index];
+                                }
+                            }
+                            output_data[outIndex] = sumValue / count;
+                        }
+                    }
+                    input_data += input_channel_size;
+                    output_data += output_channel_size;
+                }
+            }
+            return true;
+        }
+
+        template<typename T>
         static void cpu_pooling2d_compute_run(
                 const Tensor &x, Pooling2DType type, const Padding2D &padding,
                 Padding2DType padding_type, const Size2D &ksize, const Stride2D &stride,
                 Conv2DFormat format, Tensor &out) {
-            if (type == Pooling2DType::MAX) {
+#define ENCODE(a, b) ((int(a) << 8) | int(b))
+            auto code = ENCODE(padding_type, type);
+            static const auto BLACK_MAX = ENCODE(Padding2DType::BLACK, Pooling2DType::MAX);
+            static const auto BLACK_AVG = ENCODE(Padding2DType::BLACK, Pooling2DType::AVG);
+            static const auto WHITE_MAX = ENCODE(Padding2DType::WHITE, Pooling2DType::MAX);
+            static const auto WHITE_AVG = ENCODE(Padding2DType::WHITE, Pooling2DType::AVG);
+
+            if (code == BLACK_MAX) {
                 cpu_max_pooling(x.data<T>(), out.data<T>(), x.sizes(), out.sizes(),
                                 ksize, stride, padding);
-            } else if (type == Pooling2DType::AVG) {
+            } else if (code == BLACK_AVG) {
                 cpu_average_pooling(x.data<T>(), out.data<T>(), x.sizes(), out.sizes(),
                                     ksize, stride, padding);
+            } else if (code == WHITE_MAX) {
+                cpu_max_pooling(x.data<T>(), out.data<T>(), x.sizes(), out.sizes(),
+                                    ksize, stride, padding);
+            } else if (code == WHITE_AVG) {
+                cpu_average_pooling_white(x.data<T>(), out.data<T>(), x.sizes(), out.sizes(),
+                                          ksize, stride, padding);
             } else {
-                TS_LOG_ERROR << "Pooling type only support MAX and AVG" << eject;
+                TS_LOG_ERROR << "Pooling type only support MAX and AVG with BLACK and WHITE padding" << eject;
             }
+#undef ENCODE
         }
 
         void Pooling2DCore::pooling2d(const Tensor &x, Pooling2DType type, const Padding2D &padding,
@@ -133,8 +188,8 @@ namespace ts {
             if (format != FORMAT_NCHW) {
                 TS_LOG_ERROR << "Pooling2D only support NCHW" << eject;
             }
-            if (padding_type != Padding2DType::BLACK) {
-                TS_LOG_ERROR << "Pooling2D only support black padding" << eject;
+            if (padding_type != Padding2DType::BLACK && padding_type != Padding2DType::WHITE) {
+                TS_LOG_ERROR << "Pooling2D only support black padding or white padding" << eject;
             }
             DTYPE dtype = out.dtype();
             switch (dtype) {
