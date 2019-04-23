74586d690355a2dedaf7a195855f4db456f2272d
wangquyuan <quyuan.wang@seetatech.com>
2019-04-23 19:13:32 +0800

    move run_const_nodes to compiler

--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -24,6 +24,10 @@
 #include "core/device_context.h"
 #include "utils/ctxmgr_lite.h"
 
+#include "module/menu.h"
+#include "frontend/intime.h"
+
+
 
 namespace ts {
     template <typename K, typename V>
@@ -329,4 +333,85 @@ namespace ts {
 
         return block;
     }
+
+    static int run_const_node(const Node &node, Node &compute_node) {
+        std::vector<Node> inputs = node.inputs();
+        std::vector<Node> new_inputs;
+        int  input_num = inputs.size();
+        int const_inputs = 0;
+        for(int i=0; i<input_num; i++) {
+            auto &bubble = inputs[i].bubble();
+            if(bubble.op() == Bubble::Variable) {
+                TS_LOG_ERROR << "Not support " << Bubble::Variable << " in this version" << eject;
+            }else if(bubble.op() == Bubble::Const) {
+                new_inputs.push_back(inputs[i]);
+                const_inputs++;
+            }else if(bubble.op() == Bubble::Parameter) {
+                new_inputs.push_back(inputs[i]);
+            }else {
+                std::vector<Node> op_inputs = inputs[i].inputs();
+                int op_const_inputs = 0;
+
+                for(int k=0; k<op_inputs.size(); k++) {
+                     Node tmpnode = op_inputs[k];
+                     int n = run_const_node(op_inputs[k], tmpnode);
+                     if(n > 0) {
+                         op_inputs[k] = tmpnode;
+                         op_const_inputs++;
+                     }
+                }
+
+                if((op_const_inputs > 0) && (op_const_inputs == op_inputs.size())) {
+                    std::vector<Tensor> values;
+                    for(int k=0; k<op_const_inputs; k++) {
+                        auto &opbubble = op_inputs[k].bubble();
+                        TS_AUTO_CHECK(opbubble.op() == Bubble::Const);
+                        values.push_back(opbubble.get(name::value));
+                    }
+
+                    Tensor result = intime::run(bubble, values);
+                    auto data = ts::bubble::data(bubble.name(), result);
+                    const_inputs++;
+                    new_inputs.push_back(data);
+                }else
+                {
+                    new_inputs.push_back(inputs[i]);
+                }
+            }
+        }
+
+        Node::Link(compute_node, new_inputs);
+
+        if(compute_node.bubble().op() == Bubble::Const) {
+            return 1;
+        }
+
+        if((const_inputs > 0) && (const_inputs == input_num)) {
+            std::vector<Tensor> vecs;
+            for(int i=0; i<input_num; i++ ) {
+                auto &bubble = new_inputs[i].bubble();
+                TS_AUTO_CHECK(bubble.op() == Bubble::Const);
+                vecs.push_back(bubble.get(name::value));
+            }
+
+            Tensor result = intime::run(node.bubble(), vecs);
+            auto data = ts::bubble::data(node.bubble().name(), result);
+            compute_node = data;
+            return 1;
+        }
+
+        return 0;
+    }
+
+
+    void Compiler::run_const_nodes(const std::vector<Node> &nodes, std::vector<Node> &output_nodes) {
+        for(int i=0; i<nodes.size();  i++) {
+            Node tmpnode(nodes[i]);
+            run_const_node(nodes[i], tmpnode);
+            output_nodes.push_back(tmpnode);
+        }
+    }
+
+
+
 }
