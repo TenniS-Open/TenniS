8124793c60b7f4d9a1e67b924d390036e11c5369
kaizhou.li <kaizhou.li@seetatech.com>
2019-04-26 20:36:02 +0800

    format code

--- src/kernels/cpu/im2col.cpp
+++ src/kernels/cpu/im2col.cpp
@@ -77,34 +77,34 @@ void im2col_cpu(const Dtype* data_im, const int channels,
     } else {
         auto col_size = kernel_h * kernel_w * output_h * output_w;
 #ifdef TS_USE_OPENMP
-        #pragma omp parallel for num_threads(omp_threads)
+#pragma omp parallel for num_threads(omp_threads)
 #endif
         for (int channel = 0; channel < channels; ++channel) {
-                auto local_data_im = data_im + channel * channel_size;
-                auto local_data_col = data_col + channel * col_size;
-                for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
-                    for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
-                        int input_row = -pad_h_top + kernel_row * dilation_h;
-                        for (int output_rows = output_h; output_rows; output_rows--) {
-                            if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
-                                for (int output_cols = output_w; output_cols; output_cols--) {
+            auto local_data_im = data_im + channel * channel_size;
+            auto local_data_col = data_col + channel * col_size;
+            for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
+                for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
+                    int input_row = -pad_h_top + kernel_row * dilation_h;
+                    for (int output_rows = output_h; output_rows; output_rows--) {
+                        if (!is_a_ge_zero_and_a_lt_b(input_row, height)) {
+                            for (int output_cols = output_w; output_cols; output_cols--) {
+                                *(local_data_col++) = padding_value;//0;
+                            }
+                        } else {
+                            int input_col = -pad_w_left + kernel_col * dilation_w;
+                            for (int output_col = output_w; output_col; output_col--) {
+                                if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
+                                    *(local_data_col++) = local_data_im[input_row * width + input_col];
+                                } else {
                                     *(local_data_col++) = padding_value;//0;
                                 }
-                            } else {
-                                int input_col = -pad_w_left + kernel_col * dilation_w;
-                                for (int output_col = output_w; output_col; output_col--) {
-                                    if (is_a_ge_zero_and_a_lt_b(input_col, width)) {
-                                        *(local_data_col++) = local_data_im[input_row * width + input_col];
-                                    } else {
-                                        *(local_data_col++) = padding_value;//0;
-                                    }
-                                    input_col += stride_w;
-                                }
+                                input_col += stride_w;
                             }
-                            input_row += stride_h;
                         }
+                        input_row += stride_h;
                     }
                 }
+            }
         }
     }
 }
