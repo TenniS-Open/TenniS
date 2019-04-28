510c8f6a5eb483f81ac7eb14f5f178d1cdfa74d9
chaoyang <chao.yang@seetatech.com>
2019-04-28 10:05:52 +0800

    delete opt device

--- src/kernels/opt/conv2d.cpp
+++
@@ -1,7 +0,0 @@
-#include <kernels/opt/conv2d.h>
-#include <global/operator_factory.h>
-#include <backend/name.h>
-
-using namespace ts;
-using namespace opt;
-TS_REGISTER_OPERATOR(Conv2D, OPT, name::layer::conv2d())
