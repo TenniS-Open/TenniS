ddae9ef5dd90a5245d6f63448e23d9bb1e539d1f
chaoyang <chao.yang@seetatech.com>
2019-04-29 13:05:30 +0800

    add support for avx instruction

--- cmake/FLAGS_MSVC.cmake
+++ cmake/FLAGS_MSVC.cmake
@@ -3,19 +3,11 @@
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W3")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3")
 
-message(STATUS "Platform: ${PLATFORM}")
-
-
-SET(CUDA_PROPAGATE_HOST_FLAGS OFF)
-set(CUDA_ARCH  	-gencode arch=compute_30,code=sm_30
-                -gencode arch=compute_35,code=sm_35
-	            -gencode arch=compute_50,code=sm_50
-                -gencode arch=compute_60,code=sm_60)
-set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} ${CUDA_ARCH} -std=c++11")
-set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${CUDA_ARCH} -std=c++11")
-if (${CONFIGURATION} STREQUAL "Debug")
-    set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -O0 -g ")
+if(TS_USE_AVX)
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /arch:AVX")
 endif()
+
+message(STATUS "Platform: ${PLATFORM}")
 
 if ("${PLATFORM}" STREQUAL "x86")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
