959683216e3723d46b64cf58ba1ee841caba5ce6
kaizhou.li <kaizhou.li@seetatech.com>
2019-06-13 21:09:58 +0800

    add more converter support more type of layers

--- python/stackbuilder/mxnet/converter.py
+++ python/stackbuilder/mxnet/converter.py
@@ -13,6 +13,14 @@ import tensorstack as ts
 import numpy
 
 
+layer2converter = {
+}
+
+
+def register_layer_converter(layer, converter):
+    layer2converter[layer] = converter
+
+
 def convert(model_prefix, epoch, output_file,
             input_shapes=None, input_nodes=None,
             output_node_names=None):
@@ -93,6 +101,7 @@ def convert(model_prefix, epoch, output_file,
         "Flatten": convert_flatten,
         "FullyConnected": convert_fully_connected,
     }
+    converter_map.update(layer2converter)
 
     ts_nodes = [None] * len(graph.nodes)
 
@@ -265,13 +274,16 @@ def convert_convolution(node, inputs):
     if activation is not None:
         raise NotImplementedError("activation = {}".format(activation))
 
-    assert weights_blob.shape[0] * num_group == num_filter
-
     if num_group != 1 and weights_blob.shape[1] != 1:
         raise NotImplementedError("group = {} with weights.shape[1] = {}".format(num_group, weights_blob.shape[1]))
 
+    assert weights_blob.shape[0] == num_filter
+
     is_conv2d = num_group == 1
     is_depthwise_conv2d = weights_blob.shape[1] == 1
+
+    if is_depthwise_conv2d:
+        assert weights_blob.shape[1] * num_group == num_filter
 
     node = None
 
@@ -284,11 +296,11 @@ def convert_convolution(node, inputs):
     elif is_depthwise_conv2d:
         weights_shape = weights_blob.shape
         depthwise_weights_shape = (weights_shape[1], weights_shape[0], weights_shape[2], weights_shape[3])
-        weights_blob = weights_blob.reshape(shape=depthwise_weights_shape)
+        weights_blob = numpy.reshape(weights_blob, depthwise_weights_shape)
         node = ts.zoo.depthwise_conv2d(conv2d_name, x=x, w=weights_blob, format=ts.zoo.Name.NCHW,
                                        padding=[[0, 0], [0, 0], [padding[0], padding[0]], [padding[1], padding[1]]],
                                        padding_value=0,
-                                       stride=[0, 0, stride[0], stride[1]],
+                                       stride=[1, 1, stride[0], stride[1]],
                                        dilation=[1, 1, dilation[0], dilation[1]])
 
     if node is None:
@@ -448,6 +460,8 @@ def convert_fully_connected(node, inputs):
     except Exception, e:
         pass
 
+    x = ts.zoo.flatten(name=node_name + "_flatten", x=x)
+
     node = None
     if weight_value is None:
         weight_t = ts.zoo.transpose(name="_t_" + node_name, x=weight)
@@ -461,3 +475,168 @@ def convert_fully_connected(node, inputs):
     node.name = node_name
 
     return node
+
+
+def convert_copy(node, inputs):
+    # type: (dict, List[ts.Node]) -> ts.Node
+    print("--# -=[ Converting {} layer: {} ]=-".format(node["op"], node["name"]))
+
+    assert len(inputs) == 1
+
+    node_name = node['name']
+
+    attrs = node['attrs']
+
+    node = ts.zoo.copy(name=node_name, x=inputs[0])
+
+    return node
+
+
+register_layer_converter("_copy", convert_copy)
+
+
+def convert_minus_scalar(node, inputs):
+    # type: (dict, List[ts.Node]) -> ts.Node
+    print("--# -=[ Converting {} layer: {} ]=-".format(node["op"], node["name"]))
+
+    assert len(inputs) == 1
+
+    node_name = node['name']
+
+    attrs = node['attrs']
+
+    scalar = float(attrs['scalar'])
+
+    node = ts.zoo.sub(name=node_name, lhs=inputs[0], rhs=scalar, dtype=numpy.float32)
+
+    return node
+
+
+register_layer_converter("_minus_scalar", convert_minus_scalar)
+
+
+def convert_mul_scalar(node, inputs):
+    # type: (dict, List[ts.Node]) -> ts.Node
+    print("--# -=[ Converting {} layer: {} ]=-".format(node["op"], node["name"]))
+
+    assert len(inputs) == 1
+
+    node_name = node['name']
+
+    attrs = node['attrs']
+
+    scalar = float(attrs['scalar'])
+
+    node = ts.zoo.mul(name=node_name, lhs=inputs[0], rhs=scalar, dtype=numpy.float32)
+
+    return node
+
+
+register_layer_converter("_mul_scalar", convert_mul_scalar)
+
+
+def convert_contrib_resize2d(node, inputs):
+    # type: (dict, List[ts.Node]) -> ts.Node
+    print("--# -=[ Converting {} layer: {} ]=-".format(node["op"], node["name"]))
+
+    assert len(inputs) == 1
+
+    node_name = node['name']
+
+    attrs = node['attrs']
+
+    height = int(attrs['height'])
+    width = int(attrs['width'])
+
+    node = ts.zoo.resize2d(name=node_name, x=inputs[0], size=[-1, -1, height, width],
+                           type=ts.zoo.Type.resize2d_type.linear)
+
+    return node
+
+
+register_layer_converter("_contrib_BilinearResize2D", convert_contrib_resize2d)
+
+
+def convert_LeakyReLU(node, inputs):
+    # type: (dict, List[ts.Node]) -> ts.Node
+    print("--# -=[ Converting {} layer: {} ]=-".format(node["op"], node["name"]))
+
+    assert len(inputs) == 2
+
+    node_name = node['name']
+
+    attrs = node['attrs']
+
+    act_type = attrs['act_type']
+    if act_type != "prelu":
+        raise NotImplementedError("act_type = {}".format(act_type))
+
+    a = ts.zoo.to_const(inputs[1])
+
+    node = ts.zoo.prelu(name=node_name, x=inputs[0], dim=1, slope=inputs[1])
+
+    return node
+
+
+register_layer_converter("LeakyReLU", convert_LeakyReLU)
+
+
+def convert_Concat(node, inputs):
+    # type: (dict, List[ts.Node]) -> ts.Node
+    print("--# -=[ Converting {} layer: {} ]=-".format(node["op"], node["name"]))
+
+    # assert len(inputs) == 2
+
+    node_name = node['name']
+
+    attrs = node['attrs']
+
+    dim = int(attrs['dim'])
+
+    num_args = len(inputs)
+    if 'num_args' in attrs:
+        num_args = int(attrs['num_args'])
+
+    assert len(inputs) == num_args
+
+    node = ts.zoo.concat(name=node_name, inputs=inputs, dim=dim)
+
+    return node
+
+
+register_layer_converter("Concat", convert_Concat)
+
+
+register_layer_converter("Dropout", convert_copy)
+
+
+def convert_l2_norm(node, inputs):
+    # type: (dict, List[ts.Node]) -> ts.Node
+    print("--# -=[ Converting {} layer: {} ]=-".format(node["op"], node["name"]))
+
+    assert len(inputs) == 1
+
+    node_name = node['name']
+
+    attrs = node['attrs']
+
+    mode = attrs['mode']
+
+    if mode != 'instance':
+        raise NotImplementedError("mode = {}".format(mode))
+    eps = 1.00000001e-10
+    if 'eps' in attrs:
+        eps = float(attrs['eps'])
+
+    x = inputs[0]
+    x_shape = ts.zoo.shape(name=node_name + "_shape", x=x)
+    x_flatten = ts.zoo.flatten(name=node_name + "_flatten", x=x)
+    x_l2 = ts.zoo.l2_norm(name=node_name + "_L2", x=x_flatten, dim=-1, epsilon=eps)
+    node = ts.zoo.reshape_v2(name=node_name, x=x_l2, shape=x_shape)
+
+    return node
+
+
+register_layer_converter("L2Normalization", convert_l2_norm)
+
+
