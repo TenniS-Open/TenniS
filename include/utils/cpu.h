2cbdd46f4a71e4d21a752ea7f28210869236ca1a
chaoyang <chao.yang@seetatech.com>
2019-07-30 15:24:45 +0800

    init cpu enable power mode

---
+++ include/utils/cpu.h
@@ -0,0 +1,29 @@
+#ifndef TENSORSTACK_UTILS_CPU_H
+#define TENSORSTACK_UTILS_CPU_H
+
+#include "platform.h"
+#include "utils/api.h"
+
+namespace ts{
+
+    class TS_DEBUG_API CpuEnable{
+
+    public:
+        enum CpuPowerMode{
+            BALANCE = 0,
+            BIGCORE = 1,
+            LITTLECORE = 2
+        };  
+
+    public:
+        CpuEnable(){}
+        ~CpuEnable(){}
+
+    public:
+        static int get_cpu_num();
+        static bool set_power_mode(CpuPowerMode mode);
+        static CpuPowerMode get_power_mode();
+    };
+}
+
+#endif //TENSORSTACK_UTILS_CPU_H
