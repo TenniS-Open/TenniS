92fbec6fbe9e74e2b749d0515459c17ae4a56171
chaoyang <chao.yang@seetatech.com>
2019-09-09 16:10:17 +0800

    optimize pad on NCHW or NHWC format,support for various forms of pad and cut operations

---
+++ include/kernels/cpu/pad2d_algorithm.h
@@ -0,0 +1,40 @@
+#ifndef TENSORSTACK_KERNELS_CPU_PAD2D_ALGORITHM_H
+#define TENSORSTACK_KERNELS_CPU_PAD2D_ALGORITHM_H
+
+#include <core/tensor.h>
+#ifdef TS_USE_OPENMP
+#include <kernels/common/openmp.h>
+#endif
+
+namespace ts{
+    namespace cpu{
+
+        template<typename T>
+        class TS_DEBUG_API PadAlgorithm {
+        public:
+
+            //NOTE:Only supports pad on NCHW or NHWC formats.
+            static void pad_nchw_nhwc(const Tensor &x,
+                                 const std::vector<std::array<int, 2>> &padding,
+                                 float padding_value,
+                                 Tensor &out);
+
+            //TODO: Delete these superseded function
+            //NOTE:pad2d_Superseded and cut2d_Superseded only support NCHW pad on [H,W] now.
+            //pad2d don't support negative pad,cut2d support negative pad only.
+            static void pad2d_Superseded(const Tensor &x,
+                                         const std::array<int, 2> &padding_h,
+                                         const std::array<int, 2> &padding_w,
+                                         float padding_value,
+                                         Tensor &out);
+
+            static void cut2d_Superseded(const Tensor &x,
+                                         const std::array<int, 2> &padding_h,
+                                         const std::array<int, 2> &padding_w,
+                                         float padding_value,
+                                         Tensor &out);
+        };
+    }//cpu
+}//ts
+
+#endif
