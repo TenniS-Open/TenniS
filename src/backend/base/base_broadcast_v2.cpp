a379ca8a123a1ef73286657e399fcf6a640108de
kaizhou.li <kaizhou.li@seetatech.com>
2019-10-31 11:19:58 +0800

    using same reduce method like elementwise to optimize

--- src/backend/base/base_broadcast_v2.cpp
+++ src/backend/base/base_broadcast_v2.cpp
@@ -46,6 +46,50 @@ namespace ts {
             this->broadcast(x, out);
         }
 
+        static bool reduce_shape(Shape &lhs, Shape &rhs, Shape &out) {
+            bool reduced = false;
+            bool lhs_ones = false;
+            bool rhs_ones = false;
+            bool lhs_equal_rhs = false;
+            for (size_t i = 0; i < out.size(); ) {
+                lhs_ones = lhs[i] == 1;
+                rhs_ones = rhs[i] == 1;
+                lhs_equal_rhs = lhs[i] == rhs[i];
+                if (!(lhs_ones || rhs_ones || lhs_equal_rhs)) {
+                    ++i;
+                    continue;
+                }
+                auto j = i + 1;
+                for (; j < out.size(); ++j) {
+                    if (lhs_ones) lhs_ones = lhs[j] == 1;
+                    if (rhs_ones) rhs_ones = rhs[j] == 1;
+                    if (lhs_equal_rhs) lhs_equal_rhs = lhs[j] == rhs[j];
+                    if (!(lhs_ones || rhs_ones || lhs_equal_rhs)) break;
+                }
+                if (j - i > 1) {
+                    auto a = std::accumulate(lhs.begin() + i, lhs.begin() + j, 1, std::multiplies<int32_t>());
+                    auto b = std::accumulate(rhs.begin() + i, rhs.begin() + j, 1, std::multiplies<int32_t>());
+                    auto c = std::accumulate(out.begin() + i, out.begin() + j, 1, std::multiplies<int32_t>());
+
+                    lhs.erase(lhs.begin() + i, lhs.begin() + j);
+                    rhs.erase(rhs.begin() + i, rhs.begin() + j);
+                    out.erase(out.begin() + i, out.begin() + j);
+
+                    lhs.insert(lhs.begin() + i, a);
+                    rhs.insert(rhs.begin() + i, b);
+                    out.insert(out.begin() + i, c);
+
+                    reduced = true;
+                    ++i;
+                    continue;
+                } else {
+                    i = j + 1;
+                    continue;
+                }
+            }
+            return reduced;
+        }
+
         int BroadcastV2::run(Stack &stack) {
             TS_AUTO_CHECK(stack.size() == 2);
 
@@ -68,6 +112,14 @@ namespace ts {
 
             x = x.view(memory_device).reshape(x_shape);    // do sync, and set default data to given device
             auto out = *stack.push(out_proto, memory_device);
+
+            {
+                auto y_shape = out_shape;
+                if (reduce_shape(y_shape, x_shape, out_shape)) {
+                    x = x.reshape(x_shape);
+                    out = out.reshape(out_shape);
+                }
+            }
 
             int dim;
             if (is_scalar(x_shape)) {
