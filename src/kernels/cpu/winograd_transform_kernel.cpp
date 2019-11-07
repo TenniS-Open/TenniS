ca6955145dc9a6c91d619c381d2ba62dfa0c36d7
chaoyang <chao.yang@seetatech.com>
2019-11-07 18:49:15 +0800

    modify winograd transform op

--- src/kernels/cpu/winograd_transform_kernel.cpp
+++ src/kernels/cpu/winograd_transform_kernel.cpp
@@ -3,7 +3,7 @@
 #include "kernels/cpu/winograd_transform_kernel.h"
 #include "global/operator_factory.h"
 #include "backend/name.h"
-#include <kernels/cpu/conv2d_algorithm.h>
+#include "kernels/cpu/winograd_algorithm.h"
 
 namespace ts {
     namespace cpu {
@@ -13,12 +13,12 @@ namespace ts {
 #define DECLARE_COMPUTE_RUN(DTYPE, TYPE) \
             case DTYPE: { \
                 if (winograd_mode == F6X6_3X3) \
-                    Conv2dAlgorithm<TYPE>::conv3x3_winograd63_transform_kernel_inplace(x, out); \
+                    Conv2dWinogradAlgorithm<TYPE>::winograd_f63_transform_and_pack_kernel(x, 64, out); \
                 else if(winograd_mode == F2X2_3X3) \
-                    Conv2dAlgorithm<TYPE>::conv3x3_winograd23_transform_kernel_inplace(x, out); \
+                    Conv2dWinogradAlgorithm<TYPE>::winograd_f23_transform_and_pack_kernel(x, 16, out); \
                 break; }
                 DECLARE_COMPUTE_RUN(FLOAT32, float);
-                DECLARE_COMPUTE_RUN(FLOAT64, double);
+//                DECLARE_COMPUTE_RUN(FLOAT64, double);
 #undef DECLARE_COMPUTE_RUN
             default: {
                 TS_LOG_ERROR << this->op() << " not support data type(" << dtype << "): " << type_str(dtype) << eject;
