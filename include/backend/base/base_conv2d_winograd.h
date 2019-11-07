90f657ad07a63e38eacc45748d0dcfdc9ee89cf6
chaoyang <chao.yang@seetatech.com>
2019-11-07 18:51:00 +0800

    modify winograd conv2d op

--- include/backend/base/base_conv2d_winograd.h
+++ include/backend/base/base_conv2d_winograd.h
@@ -21,16 +21,20 @@ namespace ts{
 
             int run(ts::Stack &stack) override;
 
-            //virtual void conv2d_winograd(const Tensor &x, WinogradConv2DMode winograd_mode, const Padding2D &padding, float padding_value,
-            //    const Tensor &w, Conv2DFormat format, Tensor &out, Stack &stack) = 0;
-            virtual void conv2d_winograd(const Tensor &x, WinogradConv2DMode winograd_mode,
-                const Tensor &w, Conv2DFormat format, Tensor &out, Stack &stack) = 0;
+            virtual void conv2d_tranform_kernel(WinogradConv2DMode  winograd_mode, const Tensor &kernel, Tensor &kernel_transformed) = 0;
+
+            virtual void conv2d_winograd(const Tensor &x, WinogradConv2DMode winograd_mode, const Padding2D &padding, float padding_value,
+                const Tensor &w, Conv2DFormat format, Tensor &out, bool kernel_transformed) = 0;
+//            virtual void conv2d_winograd(const Tensor &x, WinogradConv2DMode winograd_mode,
+//                const Tensor &w, Conv2DFormat format, Tensor &out, Stack &stack) = 0;
 
         private:
             WinogradConv2DMode m_winograd_mode;
             Conv2DFormat m_format;
-            //std::valarray<int> m_padding4x2;
-            //float m_padding_value;
+            std::valarray<int> m_padding4x2;
+            float m_padding_value;
+            bool m_kernel_transformed;
+            Tensor m_k_transformed;
         };
     }
 }
--- src/kernels/cpu/conv2d_winograd.cpp
+++ src/kernels/cpu/conv2d_winograd.cpp
@@ -3,21 +3,45 @@
 #include "kernels/cpu/conv2d_winograd.h"
 #include "global/operator_factory.h"
 #include "backend/name.h"
-#include <kernels/cpu/conv2d_algorithm.h>
+#include "kernels/cpu/winograd_algorithm.h"
 
 namespace ts {
     namespace cpu{
-        template<typename T>
-        static void conv2d_winograd_forward(const Tensor &x, WinogradConv2DMode winograd_model,
-            const Tensor &w, Tensor &out, Stack &stack) {
-            auto x_shape = x.sizes();
-            auto weight_shape = w.sizes();
-            Size2D ksize(weight_shape[2], weight_shape[3]);
 
-            if (winograd_model == F6X6_3X3)
-                return Conv2dAlgorithm<T>::conv3x3_winograd63(x, w, out);
+        template <typename T>
+        static void conv2d_winograd_transform_kernel(WinogradConv2DMode winograd_mode,
+                                                     const Tensor &kernel,
+                                                     Tensor &kernel_transformed) {
+            Shape kernel_shape = kernel.sizes();
+
+            Shape kernel_transformed_shape;
+            int int_tile_width = winograd_mode == F2X2_3X3 ? 4 : 8;
+            int in_tile_size = int_tile_width * int_tile_width;
+
+            kernel_transformed_shape = {kernel_shape[0], kernel_shape[1], int_tile_width, int_tile_width };
+            kernel_transformed = Tensor(Tensor::InFlow::HOST, kernel.dtype(), kernel_transformed_shape);
+            if(winograd_mode == F2X2_3X3){
+                Conv2dWinogradAlgorithm<T>::winograd_f23_transform_and_pack_kernel(kernel, in_tile_size, kernel_transformed);
+            }
+            else{
+                Conv2dWinogradAlgorithm<T>::winograd_f63_transform_and_pack_kernel(kernel, in_tile_size, kernel_transformed);
+            }
+        }
+
+        template <typename T>
+        static void conv2d_winograd_forward(const Tensor &x,
+                                            WinogradConv2DMode winograd_model,
+                                            const Padding2D &padding,
+                                            float padding_value,
+                                            const Tensor &w,
+                                            Tensor &out,
+                                            bool kernel_transformed) {
+
+            if (winograd_model == F2X2_3X3){
+                return Conv2dWinogradAlgorithm<T>::winograd_f23(x, padding, padding_value, w, out, kernel_transformed);
+            }
             else
-                return Conv2dAlgorithm<T>::conv3x3_winograd23(x, w, out);
+                return Conv2dWinogradAlgorithm<T>::winograd_f63(x, padding, padding_value, w, out, kernel_transformed);
 
             //if (padding.bottom != 0 || padding.top != 0 || padding.left != 0 || padding.right != 0) {
             //    auto pad_operator = OperatorCreator::Query(CPU, name::layer::pad())();
@@ -46,17 +70,40 @@ namespace ts {
             //}
         }
 
-        void Conv2DWinograd::conv2d_winograd(const Tensor &x, WinogradConv2DMode winograd_mode,
-            const Tensor &w, Conv2DFormat format, Tensor &out, Stack &stack) {
+        void Conv2DWinograd::conv2d_tranform_kernel(WinogradConv2DMode winograd_mode,
+                                                    const Tensor &kernel,
+                                                    Tensor &kernel_transformed) {
+            DTYPE dtype = kernel.dtype();
+            switch (dtype) {
+#define DECLARE_COMPUTE_RUN(DTYPE, TYPE) \
+        case DTYPE: { conv2d_winograd_transform_kernel<TYPE>(winograd_mode, kernel, kernel_transformed); break; }
+                DECLARE_COMPUTE_RUN(FLOAT32, float);
+//                DECLARE_COMPUTE_RUN(FLOAT64, double);
+#undef DECLARE_COMPUTE_RUN
+                default: {
+                    TS_LOG_ERROR << "Conv2D not support data type(" << dtype << "): " << type_str(dtype) << eject;
+                    break;
+                }
+            }
+        }
+
+        void Conv2DWinograd::conv2d_winograd(const Tensor &x,
+                                             WinogradConv2DMode winograd_mode,
+                                             const Padding2D &padding,
+                                             float padding_value,
+                                             const Tensor &w,
+                                             Conv2DFormat format,
+                                             Tensor &out,
+                                             bool kernel_transformed) {
             if (format != FORMAT_NCHW) {
                 TS_LOG_ERROR << "Conv2D_Winograd only support NCHW" << eject;
             }
             DTYPE dtype = out.dtype();
             switch (dtype) {
 #define DECLARE_COMPUTE_RUN(DTYPE, TYPE) \
-        case DTYPE: { conv2d_winograd_forward<TYPE>(x, winograd_mode, w, out, stack); break; }
+        case DTYPE: { conv2d_winograd_forward<TYPE>(x, winograd_mode, padding, padding_value, w, out, kernel_transformed); break; }
                 DECLARE_COMPUTE_RUN(FLOAT32, float);
-                DECLARE_COMPUTE_RUN(FLOAT64, double);
+//                DECLARE_COMPUTE_RUN(FLOAT64, double);
 #undef DECLARE_COMPUTE_RUN
             default: {
                 TS_LOG_ERROR << "Conv2D not support data type(" << dtype << "): " << type_str(dtype) << eject;
