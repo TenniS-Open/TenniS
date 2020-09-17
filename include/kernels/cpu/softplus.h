c48b6cad774d4baa2fd9f3ac72c5410f17b6dd0d
sen <lvsen@tju.edu.cn>
2020-09-17 10:58:01 +0800

    add softplus operator

---
+++ include/kernels/cpu/softplus.h
@@ -0,0 +1,23 @@
+//
+// Created by sen on 8/26/20.
+//
+
+#ifndef TENNIS_SOFTPLUS_H
+#define TENNIS_SOFTPLUS_H
+
+#include "backend/base/base_activation.h"
+#include "operator_on_cpu.h"
+
+namespace ts {
+    namespace cpu {
+        class Softplus : public OperatorOnCPU<base::Activation> {
+        public:
+            using self = Softplus;
+            using supper = OperatorOnCPU<base::Activation>;
+
+            void active(const Tensor& x, Tensor& out) override;
+        };
+    }
+}
+
+#endif //TENNIS_SOFTPLUS_H
---
+++ src/kernels/cpu/softplus.cpp
@@ -0,0 +1,73 @@
+//
+// Created by sen on 8/26/20.
+//
+#include <kernels/cpu/softplus.h>
+#include "backend/name.h"
+#include "global/operator_factory.h"
+#include <algorithm>
+
+#include "kernels/common/simd.h"
+#ifdef TS_USE_OPENMP
+#include <kernels/common/openmp.h>
+#endif
+
+namespace ts {
+    namespace cpu {
+        template<typename T>
+        static void cpu_softplus_compute_run(const Tensor &x, Tensor &out) {
+            const T *input_data = x.data<T>();
+            T *output_data = out.data<T>();
+            int count = out.count();
+
+            std::memcpy(output_data, input_data, count * sizeof(T));
+
+            int counts = out.count();
+            for (int i = 0; i < counts; i++) {
+                T val = *input_data;
+                *output_data = std::log(std::exp(val) + 1.);
+                output_data++;
+            }
+        }
+
+        template<>
+        void cpu_softplus_compute_run<float>(const Tensor &x, Tensor &out) {
+            auto input_data = x.data<float>();
+            auto output_data = out.data<float>();
+            int counts = out.count();
+#ifdef TS_USE_OPENMP
+#pragma omp parallel for num_threads(openmp_threads())
+#endif
+            for (int i = 0; i < counts; ++i) {
+                *(output_data + i) = static_cast<float>(std::log(std::exp(*(input_data + i)) + 1.));
+            }
+        }
+
+        void Softplus::active(const Tensor &x, Tensor &out) {
+            // Notice: the all tensor' memory device are CPU, as given in running_memory_device
+            DTYPE dtype = out.dtype();
+            switch (dtype) {
+#define DECLARE_COMPUTE_RUN(DTYPE, TYPE) \
+        case DTYPE: { cpu_softplus_compute_run<TYPE>(x, out); break; }
+                DECLARE_COMPUTE_RUN(INT8, int8_t);
+                DECLARE_COMPUTE_RUN(UINT8, uint8_t);
+                DECLARE_COMPUTE_RUN(INT16, int16_t);
+                DECLARE_COMPUTE_RUN(UINT16, uint16_t);
+                DECLARE_COMPUTE_RUN(INT32, int32_t);
+                DECLARE_COMPUTE_RUN(UINT32, uint32_t);
+                DECLARE_COMPUTE_RUN(INT64, int64_t);
+                DECLARE_COMPUTE_RUN(UINT64, uint64_t);
+                DECLARE_COMPUTE_RUN(FLOAT32, float);
+                DECLARE_COMPUTE_RUN(FLOAT64, double);
+#undef DECLARE_COMPUTE_RUN
+                default: {
+                    TS_LOG_ERROR << this->op() << " not support data type(" << dtype << "): " << type_str(dtype) << eject;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+using namespace ts;
+using namespace cpu;
+TS_REGISTER_OPERATOR(Softplus, ts::CPU, "softplus")
