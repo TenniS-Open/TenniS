4c307faf6794c7fde1d4cec822cdcc563bdc35e9
sen <lvsen@tju.edu.cn>
2020-09-22 14:39:52 +0800

    add upcast dtype support

--- src/backend/base/element_wise_reduce.cpp
+++ src/backend/base/element_wise_reduce.cpp
@@ -10,6 +10,8 @@
 #include "runtime/stack.h"
 
 #include <numeric>
+#include "core/tensor_builder.h"
+#define UPCAST_DTYPE
 
 namespace ts {
     static inline void front_append_ones(Shape &shape, int count) {
@@ -68,11 +70,46 @@ namespace ts {
         supper::init();
     }
 
+#ifdef UPCAST_DTYPE
+    DTYPE upcast_dtype(Tensor &lhs, Tensor &rhs) {
+        // naive table
+        int lookup_table[15][15]{
+                {1	,1	,3	,3	,5	,5	,7	,7	,9	,10	,11	,1	,3	,5	,1},
+                {1	,2	,3	,3	,5	,6	,7	,7	,9	,10	,11	,1	,3	,5	,2},
+                {3	,3	,3	,3	,5	,5	,7	,7	,9	,10	,11	,3	,3	,5	,3},
+                {3	,3	,3	,4	,5	,6	,7	,8	,9	,10	,11	,3	,3	,5	,4},
+                {5	,5	,5	,5	,5	,5	,7	,7	,10	,10	,11	,5	,5	,5	,5},
+                {5	,6	,5	,6	,5	,6	,7	,8	,10	,10	,11	,5	,5	,5	,6},
+                {7	,7	,7	,7	,7	,7	,7	,7	,11	,11	,11	,7	,7	,7	,7},
+                {7	,7	,7	,8	,7	,8	,7	,8	,11	,11	,11	,7	,7	,7	,8},
+                {9	,9	,9	,9	,10	,10	,11	,11	,9	,10	,11	,9	,9	,10	,9},
+                {10	,10	,10	,10	,10	,10	,11	,11	,10	,10	,11	,10	,10	,11	,10},
+                {11	,11	,11	,11	,11	,11	,11	,11	,11	,11	,11	,11	,11	,11	,11},
+                {1	,1	,3	,3	,5	,5	,7	,7	,9	,10	,11	,13	,14	,15	,13},
+                {3	,3	,3	,3	,5	,5	,7	,7	,9	,10	,11	,14	,14	,15	,14},
+                {5	,5	,5	,5	,5	,5	,7	,7	,10	,11	,11	,15	,15	,15	,15},
+                {1	,2	,3	,4	,5	,6	,7	,8	,9	,10	,11	,13	,14	,15	,21}
+        };
+        if (lhs.dtype() == rhs.dtype()) return lhs.dtype();
+        return DTYPE(lookup_table[lhs.dtype()][rhs.dtype()]);
+    }
+#endif
+
     int ElementWiseReduce::infer(Stack &stack, std::vector<Tensor::Prototype> &output) {
         TS_AUTO_CHECK(stack.size() == 2);
 
         auto lhs = *stack.index(0);
         auto rhs = *stack.index(1);
+
+#ifdef UPCAST_DTYPE
+        DTYPE out_dtype = upcast_dtype(lhs, rhs);
+        if (lhs.dtype() != out_dtype) {
+            lhs = tensor::cast(out_dtype, lhs);
+        }
+        if (rhs.dtype() != out_dtype) {
+            rhs = tensor::cast(out_dtype, rhs);
+        }
+#endif
 
         if (lhs.dtype() != rhs.dtype()) {
             TS_LOG_ERROR << "[" << this->op() << ":" << this->name() << "] Can not reduce mismatch type: "
@@ -115,6 +152,17 @@ namespace ts {
 
         auto lhs = *stack.index(0);
         auto rhs = *stack.index(1);
+
+#ifdef UPCAST_DTYPE
+        DTYPE out_dtype = upcast_dtype(lhs, rhs);
+        std::cout << "[" << this->op() << ":" << this->name() << "]" << "element_wise_reduce out dtype: " << out_dtype << std::endl;
+        if (lhs.dtype() != out_dtype) {
+            lhs = tensor::cast(out_dtype, lhs);
+        }
+        if (rhs.dtype() != out_dtype) {
+            rhs = tensor::cast(out_dtype, rhs);
+        }
+#endif
 
         if (lhs.dtype() != rhs.dtype()) {
             TS_LOG_ERROR << "[" << this->op() << ":" << this->name() << "] Can not reduce mismatch type: "
