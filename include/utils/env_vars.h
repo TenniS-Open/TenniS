d86a976581bc5d832305a672f80f8ba90993885e
sen.lv <sen.lv@seetatech.com>
2021-05-17 21:48:40 +0800

    fix env_vars

--- include/utils/env_vars.h
+++ include/utils/env_vars.h
@@ -2,6 +2,7 @@
 #define TENNIS_ENV_VARS_H
 
 #include <utils/platform.h>
+#include <string>
 
 #if TS_PLATFORM_OS_WINDOWS
     #include <Windows.h>
@@ -10,18 +11,7 @@
 #endif
 
 namespace ts {
-    std::string getEnvironmentVariable(const std::string& envName) {
-#if TS_PLATFORM_OS_WINDOWS
-        #include <Windows.h>
-        char buffer[MAX_PATH];
-        DWORD code = GetEnvironmentVariable(envName.c_str(), buffer, MAX_PATH);
-        return code == 0 ? std::string("") : std::string(buffer)
-#elif TS_PLATFORM_OS_LINUX || TS_PLATFORM_OS_ANDROID
-        char* buffer = nullptr;
-        buffer = std::getenv(envName.c_str());
-        return buffer == nullptr ? std::string("") : std::string(buffer);
-#endif
-    }
+    std::string getEnvironmentVariable(const std::string& envName);
 
 }
 
--- src/utils/env_vars.cpp
+++ src/utils/env_vars.cpp
@@ -1,5 +1,17 @@
-//
-// Created by sen on 2021/5/17.
-//
-
 #include <utils/env_vars.h>
+
+namespace ts {
+    std::string getEnvironmentVariable(const std::string& envName) {
+#if TS_PLATFORM_OS_WINDOWS
+        #include <Windows.h>
+        char buffer[MAX_PATH];
+        DWORD code = GetEnvironmentVariable(envName.c_str(), buffer, MAX_PATH);
+        return code == 0 ? std::string("") : std::string(buffer)
+#elif TS_PLATFORM_OS_LINUX || TS_PLATFORM_OS_ANDROID
+        char* buffer = nullptr;
+        buffer = std::getenv(envName.c_str());
+        return buffer == nullptr ? std::string("") : std::string(buffer);
+#endif
+    }
+
+}
