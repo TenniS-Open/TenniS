526e67d04b44eb1eb4a7950b18f5e8adfcfa963f
kaizhou.li <kaizhou.li@seetatech.com>
2021-08-26 19:51:25 +0800

    base test arm cross compile toolchains.

--- toolchain/arm-linux-gnueabihf.cmake
+++ toolchain/arm-linux-gnueabihf.cmake
@@ -3,8 +3,9 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(ARM_ARCH gnueabihf)
 
-set(CMAKE_SYSROOT /usr/arm-linux-${ARM_ARCH})
-# set(CMAKE_STAGING_PREFIX /usr/arm-linux-${ARM_ARCH}/stage)
+#set(CMAKE_SYSROOT /usr/arm-linux-${ARM_ARCH})
+set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-${ARM_ARCH})
+#set(CMAKE_STAGING_PREFIX /usr/arm-linux-${ARM_ARCH})
 
 set(CMAKE_C_COMPILER arm-linux-${ARM_ARCH}-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-${ARM_ARCH}-g++)
@@ -13,3 +14,4 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
