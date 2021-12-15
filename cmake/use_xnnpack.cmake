635056db8563ef2532be8ca4cde52f82179f04ca
sen.lv <sen.lv@seetatech.com>
2021-12-15 11:24:10 +0800

    using cmake to build xnnpack backend automatically

---
+++ cmake/use_xnnpack.cmake
@@ -0,0 +1,15 @@
+cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+
+include(FetchContent)
+FetchContent_Declare(
+        xnnpack
+        GIT_REPOSITORY https://lvsen@gitlab.seetatech.com/lvsen/XNNPACK.git
+        GIT_TAG tennis
+        )
+FetchContent_MakeAvailable(xnnpack)
+
+set_target_properties(XNNPACK PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/xnnpack")
+set_target_properties(cpuinfo PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/cpuinfo")
+set_target_properties(pthreadpool PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/pthreadpool")
+set_target_properties(clog PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/clog")
+list(APPEND third_libraries XNNPACK)
