88316072226615b74ee170cff3696b54c7aa145b
sen.lv <sen.lv@seetatech.com>
2021-12-16 10:40:14 +0800

    add xnnpack backend cmake config support begin version 3.11

--- cmake/use_xnnpack.cmake
+++ cmake/use_xnnpack.cmake
@@ -1,11 +1,22 @@
-cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.11 FATAL_ERROR)
 
 include(FetchContent)
 FetchContent_Declare(
-        xnnpack
-        GIT_REPOSITORY https://lvsen@gitlab.seetatech.com/lvsen/XNNPACK.git
-        GIT_TAG tennis
-        )
+    xnnpack
+    GIT_REPOSITORY https://lvsen@gitlab.seetatech.com/lvsen/XNNPACK.git
+    GIT_TAG tennis
+)
+
+if(${CMAKE_VERSION} VERSION_LESS 3.14)
+    macro(FetchContent_MakeAvailable NAME)
+        FetchContent_GetProperties(${NAME})
+        if(NOT ${NAME}_POPULATED)
+            FetchContent_Populate(${NAME})
+            add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
+        endif()
+    endmacro()
+endif()
+
 FetchContent_MakeAvailable(xnnpack)
 
 set_target_properties(XNNPACK PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/xnnpack")
